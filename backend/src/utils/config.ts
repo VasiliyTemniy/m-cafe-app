b42aeaef0020092b4da14af94705583d29b3d8fb
VasiliyTemniy <flash_er@mail.ru>
2023-07-20 11:39:59 +0300

    remove separate env vars for dev/prod

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -8,22 +8,13 @@ import { Secret } from 'jsonwebtoken';
 
 const PORT = process.env.BACKEND_PORT as string;
 
-const DATABASE_URL =
-  (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')
-  ? process.env.DEV_DATABASE_URL as string
-  : process.env.DATABASE_URL as string;
+const DATABASE_URL = process.env.DATABASE_URL as string;
 
-const SECRET =
-  (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')
-  ? process.env.DEV_SECRET as Secret
-  : process.env.SECRET as Secret;
+const SECRET = process.env.SECRET as Secret;
 
 const TOKEN_TTL = process.env.TOKEN_TTL as string;
 
-const ALLOWED_ORIGIN = 
-  (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')
-  ? process.env.DEV_ALLOWED_ORIGIN as string
-  : process.env.ALLOWED_ORIGIN as string;
+const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN as string;
 
 export default {
   DATABASE_URL,
