946095a551bccba25ab7156724664dbea7679a87
VasiliyTemniy <flash_er@mail.ru>
2023-07-20 12:01:23 +0300

    add super admin init at server start

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,11 @@ import app from './app.js';
 import http from 'http';
 import logger from './utils/logger.js';
 import { connectToDatabase } from './utils/db.js';
+import { initSuperAdmin } from './utils/adminInit.js';
 
 const start = async () => {
   await connectToDatabase();
+  await initSuperAdmin();
   const server = http.createServer(app);
   server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`);
---
+++ backend/src/utils/adminInit.ts
@@ -0,0 +1,37 @@
+import models from '../models/index.js';
+import bcryptjs from 'bcryptjs';
+import logger from './logger.js';
+
+const User = models.User;
+
+export const initSuperAdmin = async () => {
+
+  const existingUser = await User.findOne({
+    where: {
+      username: process.env.SUPERADMIN_USERNAME as string
+    }
+  });
+  if (existingUser) {
+    return logger.info('Super admin user already exists');
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcryptjs.hash(process.env.SUPERADMIN_PASSWORD as string, saltRounds);
+
+  const user = {
+    username: process.env.SUPERADMIN_USERNAME as string,
+    phonenumber: process.env.SUPERADMIN_PHONENUMBER as string,
+    passwordHash,
+    admin: true
+  };
+
+  await User.create(user);
+
+  if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
+    process.env.SUPERADMIN_USERNAME = '';
+    process.env.SUPERADMIN_PHONENUMBER = '';
+    process.env.SUPERADMIN_PASSWORD = '';
+  }
+
+  return logger.info('Super admin user successfully created!');
+};
