73a840ec9c14ae5941563f3db98db618731a4c54
VasiliyTemniy <flash_er@mail.ru>
2023-07-21 16:13:55 +0300

    separate DATABASE_URL for test

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -8,7 +8,9 @@ import { Secret } from 'jsonwebtoken';
 
 const PORT = process.env.BACKEND_PORT as string;
 
-const DATABASE_URL = process.env.DATABASE_URL as string;
+const DATABASE_URL = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_DATABASE_URL as string
+  : process.env.DATABASE_URL as string;
 
 const SECRET = process.env.SECRET as Secret;
 
