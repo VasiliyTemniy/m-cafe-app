c46d4e0fb44d9688af112218ec60238691c4ff2a
VasiliyTemniy <flash_er@mail.ru>
2023-07-26 19:39:49 +0300

    add session api helper - login function

---
+++ services/backend/src/tests/sessions_api_helper.ts
@@ -0,0 +1,33 @@
+import { LoginUserBody } from "@m-cafe-app/utils";
+import supertest from 'supertest';
+import { apiBaseUrl } from './test_helper';
+import { UserInDB } from "./users_api_helper";
+import { expect } from "chai";
+
+export const userAgent = 'SUPERTEST';
+
+export const initLogin = async (
+  user: UserInDB,
+  password: string,
+  api: supertest.SuperTest<supertest.Test>,
+  expectedStatus: number,
+  loginUserAgent: string = userAgent
+) => {
+
+  const loginBody: LoginUserBody = {
+    phonenumber: user.phonenumber,
+    password: password
+  };
+
+  const response = await api
+    .post(`${apiBaseUrl}/session`)
+    .set('User-Agent', loginUserAgent)
+    .send(loginBody as object)
+    .expect(expectedStatus)
+    .expect('Content-Type', /application\/json/);
+
+  if (!response.body.token) return expect(true).to.equal(false);
+
+  return response.body.token as string;
+
+};
