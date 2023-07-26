7b9cfe68d9f8568134dea32ada25179f1438ffb9
VasiliyTemniy <flash_er@mail.ru>
2023-07-26 16:07:45 +0300

    move adminCheck to middleware

--- services/backend/src/controllers/admin.ts
+++ services/backend/src/controllers/admin.ts
@@ -1,7 +1,6 @@
 import { Router, RequestHandler } from 'express';
-import { DatabaseError, ProhibitedError, RequestBodyError, UnknownError } from '@m-cafe-app/utils';
+import { DatabaseError, RequestBodyError } from '@m-cafe-app/utils';
 import { isDisableUserBody } from '@m-cafe-app/utils';
-import { isCustomRequest } from '../types/RequestCustom.js';
 import middleware from '../utils/middleware.js';
 import { User, Session } from '../models/index.js';
 
@@ -10,15 +9,9 @@ const adminRouter = Router();
 adminRouter.get(
   '/users/',
   middleware.verifyToken,
-  middleware.userExtractor,
+  middleware.adminCheck,
   middleware.sessionCheck,
   (async (req, res) => {
-
-    if (!isCustomRequest(req)) throw new UnknownError('This code should never be reached');
-
-    const userAdmin = req.user;
-
-    if (!userAdmin.admin) throw new ProhibitedError('You have no admin permissions');
 
     const userSubjects = await User.findAll({
       attributes: { exclude: ['createdAt', 'updatedAt', 'passwordHash', 'disabled', 'admin'] },
@@ -41,12 +34,6 @@ adminRouter.get(
   middleware.sessionCheck,
   (async (req, res) => {
 
-    if (!isCustomRequest(req)) throw new UnknownError('This code should never be reached');
-
-    const userAdmin = req.user;
-
-    if (!userAdmin.admin) throw new ProhibitedError('You have no admin permissions');
-
     const userSubject = await User.findByPk(req.params.id, {
       attributes: { exclude: ['createdAt', 'updatedAt', 'passwordHash', 'disabled', 'admin'] }
     });
@@ -65,12 +52,7 @@ adminRouter.put(
   middleware.sessionCheck,
   (async (req, res) => {
 
-    if (!isCustomRequest(req)) throw new UnknownError('This code should never be reached');
     if (!isDisableUserBody(req.body)) throw new RequestBodyError('Invalid disable user request body');
-
-    const userAdmin = req.user;
-
-    if (!userAdmin.admin) throw new ProhibitedError('You have no admin permissions');
 
     const userSubject = await User.scope('all').findByPk(req.params.id);
 
