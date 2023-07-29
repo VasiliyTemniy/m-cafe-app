38e7bf865506453923a7332233d220195f893e0a
VasiliyTemniy <flash_er@mail.ru>
2023-07-29 14:34:49 +0300

    change user validation to instaceof User. custom validator is not needed for class instances

--- services/backend/src/types/RequestCustom.ts
+++ services/backend/src/types/RequestCustom.ts
@@ -1,7 +1,6 @@
 import { isNumber, isString } from '@m-cafe-app/utils';
 import { Request } from 'express';
 import { User } from '../models/index.js';
-import { isUser } from '@m-cafe-app/utils';
 
 export interface RequestMiddle extends Request {
   userId?: number;
@@ -47,4 +46,4 @@ export const hasRequestWithUserFields = (req: Request): req is RequestWithUserFi
   Object.prototype.hasOwnProperty.call(req, "user");
 
 export const isRequestWithUser = (req: Request): req is RequestWithUser =>
-  hasRequestWithUserFields(req) && isNumber(req.userId) && isString(req.token) && isUser(req.user);
+  hasRequestWithUserFields(req) && isNumber(req.userId) && isString(req.token) && req.user instanceof User;
