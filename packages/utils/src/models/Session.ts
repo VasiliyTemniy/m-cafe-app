47b17c9d0e84f7d80dd12fd5907b9a899cfd3326
VasiliyTemniy <flash_er@mail.ru>
2023-07-30 20:55:32 +0300

    fix for just in case

--- packages/utils/src/models/Session.ts
+++ packages/utils/src/models/Session.ts
@@ -1,5 +1,5 @@
 import { Model, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey } from 'sequelize';
-import { User } from './User';
+import { User } from './User.js';
 
 export class Session extends Model<InferAttributes<Session>, InferCreationAttributes<Session>> {
   declare id: CreationOptional<number>;
