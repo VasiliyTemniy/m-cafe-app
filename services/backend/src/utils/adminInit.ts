81ec2bb2325475df6c07749f71f71e6315991333
VasiliyTemniy <flash_er@mail.ru>
2023-07-30 22:59:28 +0300

    another fix for just in case

--- services/backend/src/utils/adminInit.ts
+++ services/backend/src/utils/adminInit.ts
@@ -5,17 +5,39 @@ import config from './config.js';
 
 export const initSuperAdmin = async () => {
 
+  // Check for existing superadmin
   const existingUser = await User.findOne({
     where: {
-      username: process.env.SUPERADMIN_USERNAME as string
+      username: process.env.SUPERADMIN_USERNAME as string,
+      phonenumber: config.SUPERADMIN_PHONENUMBER
     }
   });
   if (existingUser) {
+
+    // Some paranoid checks
+    if (
+      !existingUser.admin
+      ||
+      existingUser.disabled
+      ||
+      existingUser.phonenumber !== config.SUPERADMIN_PHONENUMBER
+    ) {
+      existingUser.username = process.env.SUPERADMIN_USERNAME as string;
+      existingUser.phonenumber = config.SUPERADMIN_PHONENUMBER;
+      const saltRounds = 10;
+      const passwordHash = await bcryptjs.hash(process.env.SUPERADMIN_PASSWORD as string, saltRounds);
+      existingUser.passwordHash = passwordHash;
+      existingUser.admin = true;
+      existingUser.disabled = false;
+      await existingUser.save();
+    }
+
     process.env.SUPERADMIN_USERNAME = '';
     process.env.SUPERADMIN_PASSWORD = '';
     return logger.info('Super admin user already exists');
   }
 
+  // New superadmin creation
   const saltRounds = 10;
   const passwordHash = await bcryptjs.hash(process.env.SUPERADMIN_PASSWORD as string, saltRounds);
 
