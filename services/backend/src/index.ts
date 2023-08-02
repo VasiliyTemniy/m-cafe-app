85871fcf0d2d372ebb9fd46fff68d6921a7ca8b1
VasiliyTemniy <flash_er@mail.ru>
2023-08-02 17:02:56 +0300

    add redis connection

--- services/backend/src/index.ts
+++ services/backend/src/index.ts
@@ -4,10 +4,12 @@ import http from 'http';
 import logger from './utils/logger.js';
 import { connectToDatabase } from './utils/db.js';
 import { initSuperAdmin } from './utils/adminInit.js';
+import { connectToRedis } from './utils/redis.js';
 
 const start = async () => {
   await connectToDatabase();
   await initSuperAdmin();
+  await connectToRedis();
   const server = http.createServer(app);
   server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`);
