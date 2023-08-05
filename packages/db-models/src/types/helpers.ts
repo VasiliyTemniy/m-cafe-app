94def6a7cc081e2657849fc126a6d44e4c9cff3b
VasiliyTemniy <flash_er@mail.ru>
2023-08-05 15:26:49 +0300

    upd helper to provide runtime omit fields

--- packages/db-models/src/types/helpers.ts
+++ packages/db-models/src/types/helpers.ts
@@ -1,2 +1,5 @@
 export type PropertiesTimestamps = 'createdAt' | 'updatedAt' | 'deletedAt';
 export type PropertiesCreationOptional = 'id' | PropertiesTimestamps;
+
+export const timestampsKeys = ['createdAt', 'updatedAt', 'deletedAt'];
+export const creationOptionalKeys = ['id', ...timestampsKeys];
