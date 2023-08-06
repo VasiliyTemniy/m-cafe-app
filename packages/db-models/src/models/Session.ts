cc4e1636d565ab573edfc2ce5af27325d9cf31af
VasiliyTemniy <flash_er@mail.ru>
2023-08-06 13:02:35 +0300

    rm sequelize Session

--- packages/db-models/src/models/Session.ts
+++
@@ -1,9 +0,0 @@
-import { Model, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey } from 'sequelize';
-import { User } from './User.js';
-
-export class Session extends Model<InferAttributes<Session>, InferCreationAttributes<Session>> {
-  declare id: CreationOptional<number>;
-  declare userId: ForeignKey<User['id']>;
-  declare token: string;
-  declare userAgent: string;
-}
