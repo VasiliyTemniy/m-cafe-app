61d39bac2d1ff909f73dac74491a70b223217c05
VasiliyTemniy <flash_er@mail.ru>
2023-08-07 23:47:54 +0300

    add data transit types

--- packages/utils/src/models/Food.ts
+++ packages/utils/src/models/Food.ts
@@ -1,1 +1,39 @@
-export const dummyFoods = 'dummy';
+import { FoodData } from "@m-cafe-app/db-models";
+import { isNumber } from "../types/typeParsers.js";
+import { hasOwnProperty, MapToDT, MapToUnknown } from "../types/helpers.js";
+import { isLocStringDT, LocStringDT } from "./LocString.js";
+import { FoodTypeDT, isFoodTypeDT } from "./FoodType.js";
+
+
+export type FoodDT = Omit<MapToDT<FoodData>, 'nameLocId' | 'descriptionLocId' | 'foodTypeId'>
+& {
+  nameLoc: LocStringDT;
+  descriptionLoc: LocStringDT;
+  foodType: FoodTypeDT;
+};
+
+type FoodDTFields = MapToUnknown<FoodDT>;
+
+const hasFoodDTFields = (obj: unknown): obj is FoodDTFields =>
+  hasOwnProperty(obj, "id")
+  &&
+  hasOwnProperty(obj, "nameLoc")
+  &&
+  hasOwnProperty(obj, "descriptionLoc")
+  &&
+  hasOwnProperty(obj, "foodType")
+  &&
+  hasOwnProperty(obj, "price");
+
+export const isFoodDT = (obj: unknown): obj is FoodDT =>
+  hasFoodDTFields(obj)
+  &&
+  isNumber(obj.id)
+  &&
+  isLocStringDT(obj.nameLoc)
+  &&
+  isLocStringDT(obj.descriptionLoc)
+  &&
+  isFoodTypeDT(obj.foodType)
+  &&
+  isNumber(obj.price);
--- packages/utils/src/models/FoodType.ts
+++ packages/utils/src/models/FoodType.ts
@@ -1,1 +1,29 @@
-export const dummyFoodTypes = 'dummy';
+import { FoodTypeData } from "@m-cafe-app/db-models";
+import { isNumber } from "../types/typeParsers.js";
+import { hasOwnProperty, MapToDT, MapToUnknown } from "../types/helpers.js";
+import { isLocStringDT, LocStringDT } from "./LocString.js";
+
+
+export type FoodTypeDT = Omit<MapToDT<FoodTypeData>, 'nameLocId' | 'descriptionLocId'>
+& {
+  nameLoc: LocStringDT;
+  descriptionLoc: LocStringDT;
+};
+
+type FoodTypeDTFields = MapToUnknown<FoodTypeDT>;
+
+const hasFoodTypeDTFields = (obj: unknown): obj is FoodTypeDTFields =>
+  hasOwnProperty(obj, "id")
+  &&
+  hasOwnProperty(obj, "nameLoc")
+  &&
+  hasOwnProperty(obj, "descriptionLoc");
+
+export const isFoodTypeDT = (obj: unknown): obj is FoodTypeDT =>
+  hasFoodTypeDTFields(obj)
+  &&
+  isNumber(obj.id)
+  &&
+  isLocStringDT(obj.nameLoc)
+  &&
+  isLocStringDT(obj.descriptionLoc);
