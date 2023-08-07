0d3313261e148f2026e64dbe0c5c309c9265de24
VasiliyTemniy <flash_er@mail.ru>
2023-08-07 23:52:20 +0300

    add food and foodType routers

---
+++ services/backend/src/controllers/food.ts
@@ -0,0 +1,292 @@
+import { Router, RequestHandler } from 'express';
+import middleware from '../utils/middleware.js';
+import { Food, FoodType, LocString } from '../models/index.js';
+import {
+  FoodDT,
+  isNewFoodBody,
+  mapDataToTransit,
+  RequestBodyError,
+  timestampsKeys,
+  isEditFoodBody,
+  DatabaseError,
+  updateInstance,
+} from '@m-cafe-app/utils';
+
+
+const foodRouter = Router();
+
+foodRouter.get(
+  '/',
+  (async (req, res) => {
+
+    const foods = await Food.findAll({
+      attributes: {
+        exclude: [...timestampsKeys]
+      },
+      include: [
+        {
+          model: LocString,
+          as: 'nameLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        },
+        {
+          model: LocString,
+          as: 'descriptionLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        },
+        {
+          model: FoodType,
+          as: 'foodType',
+          attributes: {
+            exclude: [...timestampsKeys]
+          },
+          include: [
+            {
+              model: LocString,
+              as: 'nameLoc',
+              attributes: {
+                exclude: [...timestampsKeys]
+              }
+            },
+            {
+              model: LocString,
+              as: 'descriptionLoc',
+              attributes: {
+                exclude: [...timestampsKeys]
+              }
+            },
+          ]
+        }
+      ]
+    });
+
+    const resBody: FoodDT[] = foods.map(food => {
+      return {
+        id: food.id,
+        nameLoc: mapDataToTransit(food.nameLoc!.dataValues),
+        descriptionLoc: mapDataToTransit(food.descriptionLoc!.dataValues),
+        price: food.price,
+        foodType: {
+          id: food.foodType!.id,
+          nameLoc: mapDataToTransit(food.foodType!.nameLoc!.dataValues),
+          descriptionLoc: mapDataToTransit(food.foodType!.descriptionLoc!.dataValues)
+        }
+      };
+    });
+
+    res.status(200).json(resBody);
+
+  }) as RequestHandler
+);
+
+foodRouter.get(
+  '/:id',
+  (async (req, res) => {
+
+    const food = await Food.findByPk(req.params.id, {
+      attributes: {
+        exclude: [...timestampsKeys]
+      },
+      include: [
+        {
+          model: LocString,
+          as: 'nameLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        },
+        {
+          model: LocString,
+          as: 'descriptionLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        },
+        {
+          model: FoodType,
+          as: 'foodType',
+          attributes: {
+            exclude: [...timestampsKeys]
+          },
+          include: [
+            {
+              model: LocString,
+              as: 'nameLoc',
+              attributes: {
+                exclude: [...timestampsKeys]
+              }
+            },
+            {
+              model: LocString,
+              as: 'descriptionLoc',
+              attributes: {
+                exclude: [...timestampsKeys]
+              }
+            },
+          ]
+        }
+      ]
+    });
+
+    if (!food) throw new DatabaseError(`No food type entry with this id ${req.params.id}`);
+
+    const resBody: FoodDT = {
+      id: food.id,
+      nameLoc: mapDataToTransit(food.nameLoc!.dataValues),
+      descriptionLoc: mapDataToTransit(food.descriptionLoc!.dataValues),
+      price: food.price,
+      foodType: {
+        id: food.foodType!.id,
+        nameLoc: mapDataToTransit(food.foodType!.nameLoc!.dataValues),
+        descriptionLoc: mapDataToTransit(food.foodType!.descriptionLoc!.dataValues)
+      }
+    };
+
+    res.status(200).json(resBody);
+
+  }) as RequestHandler
+);
+
+foodRouter.post(
+  '/',
+  middleware.verifyToken,
+  middleware.adminCheck,
+  middleware.sessionCheck,
+  (async (req, res) => {
+
+    if (!isNewFoodBody(req.body)) throw new RequestBodyError('Invalid add food request body');
+
+    const { nameLoc, descriptionLoc, price, foodTypeId } = req.body;
+
+    const existingFoodType = await FoodType.findByPk(foodTypeId, {
+      attributes: {
+        exclude: [...timestampsKeys]
+      },
+      include: [
+        {
+          model: LocString,
+          as: 'nameLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        },
+        {
+          model: LocString,
+          as: 'descriptionLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        }
+      ]
+    });
+
+    if (!existingFoodType) throw new DatabaseError(`No food type entry with this id ${foodTypeId}`); 
+
+    const savedNameLoc = await LocString.create(nameLoc);
+    const savedDescriptionLoc = await LocString.create(descriptionLoc);
+
+    const savedFood = await Food.create({
+      nameLocId: savedNameLoc.id,
+      descriptionLocId: savedDescriptionLoc.id,
+      price,
+      foodTypeId
+    });
+
+    const resBody: FoodDT = {
+      id: savedFood.id,
+      nameLoc: mapDataToTransit(savedNameLoc.dataValues),
+      descriptionLoc: mapDataToTransit(savedDescriptionLoc.dataValues),
+      price,
+      foodType: {
+        id: foodTypeId,
+        nameLoc: mapDataToTransit(existingFoodType.nameLoc!.dataValues),
+        descriptionLoc: mapDataToTransit(existingFoodType.descriptionLoc!.dataValues)
+      }
+    };
+
+    res.status(201).json(resBody);
+
+  }) as RequestHandler
+);
+
+foodRouter.put(
+  '/:id',
+  middleware.verifyToken,
+  middleware.adminCheck,
+  middleware.sessionCheck,
+  (async (req, res) => {
+
+    if (!isEditFoodBody(req.body)) throw new RequestBodyError('Invalid add food request body');
+
+    const { nameLoc, descriptionLoc, price, foodTypeId } = req.body;
+
+    const updFood = await Food.findByPk(req.params.id);
+    if (!updFood) throw new DatabaseError(`No food type entry with this id ${req.params.id}`);
+
+    const updFoodType = await FoodType.findByPk(foodTypeId, {
+      include: [
+        {
+          model: LocString,
+          as: 'nameLoc',
+        },
+        {
+          model: LocString,
+          as: 'descriptionLoc',
+        }
+      ]
+    });
+    if (!updFoodType) throw new DatabaseError(`No food type entry with this id ${foodTypeId}`);
+
+    const updNameLoc = await LocString.findOne({ where: { id: nameLoc.id } });
+    if (!updNameLoc) throw new DatabaseError(`No localization entry with this id ${nameLoc.id}`);
+
+    const updDescriptionLoc = await LocString.findOne({ where: { id: descriptionLoc.id } });
+    if (!updDescriptionLoc) throw new DatabaseError(`No localization entry with this id ${descriptionLoc.id}`);
+
+    updateInstance(updNameLoc, nameLoc);
+    updateInstance(updDescriptionLoc, descriptionLoc);
+
+    await updNameLoc.save();
+    await updDescriptionLoc.save();
+
+    updFood.price = price;
+    updFood.foodTypeId = foodTypeId;
+
+    await updFood.save();
+
+    const resBody: FoodDT = {
+      id: updFood.id,
+      nameLoc: mapDataToTransit(updNameLoc.dataValues),
+      descriptionLoc: mapDataToTransit(updDescriptionLoc.dataValues),
+      price,
+      foodType: {
+        id: foodTypeId,
+        nameLoc: mapDataToTransit(updFoodType.nameLoc!.dataValues),
+        descriptionLoc: mapDataToTransit(updFoodType.descriptionLoc!.dataValues)
+      }
+    };
+
+    res.status(200).json(resBody);
+
+  }) as RequestHandler
+);
+
+foodRouter.delete(
+  '/:id',
+  middleware.verifyToken,
+  middleware.adminCheck,
+  middleware.sessionCheck,
+  (async (req, res) => {
+
+    await Food.destroy({ where: { id: req.params.id } });
+
+    res.status(204).end();
+
+  }) as RequestHandler
+);
+
+export default foodRouter;
---
+++ services/backend/src/controllers/foodType.ts
@@ -0,0 +1,174 @@
+import { Router, RequestHandler } from 'express';
+import middleware from '../utils/middleware.js';
+import { FoodType, LocString } from '../models/index.js';
+import {
+  FoodTypeDT,
+  isNewFoodTypeBody,
+  mapDataToTransit,
+  RequestBodyError,
+  timestampsKeys,
+  DatabaseError,
+  updateInstance,
+  isEditFoodTypeBody
+} from '@m-cafe-app/utils';
+
+
+const foodTypeRouter = Router();
+
+foodTypeRouter.get(
+  '/',
+  (async (req, res) => {
+
+    const foodTypes = await FoodType.findAll({
+      attributes: {
+        exclude: [...timestampsKeys]
+      },
+      include: [
+        {
+          model: LocString,
+          as: 'nameLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        },
+        {
+          model: LocString,
+          as: 'descriptionLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        }
+      ]
+    });
+
+    const resBody: FoodTypeDT[] = foodTypes.map(foodType => {
+      return {
+        id: foodType.id,
+        nameLoc: mapDataToTransit(foodType.nameLoc!.dataValues),
+        descriptionLoc: mapDataToTransit(foodType.descriptionLoc!.dataValues)
+      };
+    });
+
+    res.status(200).json(resBody);
+
+  }) as RequestHandler
+);
+
+foodTypeRouter.get(
+  '/:id',
+  (async (req, res) => {
+
+    const foodType = await FoodType.findByPk(req.params.id, {
+      attributes: {
+        exclude: [...timestampsKeys]
+      },
+      include: [
+        {
+          model: LocString,
+          as: 'nameLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        },
+        {
+          model: LocString,
+          as: 'descriptionLoc',
+          attributes: {
+            exclude: [...timestampsKeys]
+          }
+        }
+      ]
+    });
+
+    if (!foodType) throw new DatabaseError(`No food type entry with this id ${req.params.id}`);
+
+    const resBody: FoodTypeDT = {
+      id: foodType.id,
+      nameLoc: mapDataToTransit(foodType.nameLoc!.dataValues),
+      descriptionLoc: mapDataToTransit(foodType.descriptionLoc!.dataValues)
+    };
+
+    res.status(200).json(resBody);
+
+  }) as RequestHandler
+);
+
+foodTypeRouter.post(
+  '/',
+  middleware.verifyToken,
+  middleware.adminCheck,
+  middleware.sessionCheck,
+  (async (req, res) => {
+
+    if (!isNewFoodTypeBody(req.body)) throw new RequestBodyError('Invalid add food type request body');
+
+    const { nameLoc, descriptionLoc } = req.body;
+
+    const savedNameLoc = await LocString.create(nameLoc);
+    const savedDescriptionLoc = await LocString.create(descriptionLoc);
+
+    const savedFoodType = await FoodType.create({
+      nameLocId: savedNameLoc.id,
+      descriptionLocId: savedDescriptionLoc.id
+    });
+
+    const resBody: FoodTypeDT = {
+      id: savedFoodType.id,
+      nameLoc: mapDataToTransit(savedNameLoc.dataValues),
+      descriptionLoc: mapDataToTransit(savedDescriptionLoc.dataValues)
+    };
+
+    res.status(201).json(resBody);
+
+  }) as RequestHandler
+);
+
+foodTypeRouter.put(
+  '/:id',
+  middleware.verifyToken,
+  middleware.adminCheck,
+  middleware.sessionCheck,
+  (async (req, res) => {
+
+    if (!isEditFoodTypeBody(req.body)) throw new RequestBodyError('Invalid add food type request body');
+
+    const { nameLoc, descriptionLoc } = req.body;
+
+    const updFoodType = await FoodType.findByPk(req.params.id);
+    if (!updFoodType) throw new DatabaseError(`No food type entry with this id ${req.params.id}`);
+
+    const updNameLoc = await LocString.findOne({ where: { id: nameLoc.id } });
+    if (!updNameLoc) throw new DatabaseError(`No localization entry with this id ${nameLoc.id}`);
+
+    const updDescriptionLoc = await LocString.findOne({ where: { id: descriptionLoc.id } });
+    if (!updDescriptionLoc) throw new DatabaseError(`No localization entry with this id ${descriptionLoc.id}`);
+
+    updateInstance(updNameLoc, nameLoc);
+    updateInstance(updDescriptionLoc, descriptionLoc);
+
+    const resBody: FoodTypeDT = {
+      id: updFoodType.id,
+      nameLoc: mapDataToTransit(updNameLoc.dataValues),
+      descriptionLoc: mapDataToTransit(updDescriptionLoc.dataValues)
+    };
+
+    res.status(200).json(resBody);
+
+  }) as RequestHandler
+);
+
+foodTypeRouter.delete(
+  '/:id',
+  middleware.verifyToken,
+  middleware.adminCheck,
+  middleware.sessionCheck,
+  (async (req, res) => {
+
+    await FoodType.destroy({ where: { id: req.params.id } });
+
+    res.status(204).end();
+
+  }) as RequestHandler
+);
+
+export default foodTypeRouter;
