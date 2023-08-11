3e373eb1d535757596de46087ba444c750fa92b1
VasiliyTemniy <flash_er@mail.ru>
2023-08-11 16:44:03 +0300

    change facilityManager to a pure junction table

--- packages/db-models/src/models/FacilityManager.ts
+++ packages/db-models/src/models/FacilityManager.ts
@@ -1,11 +1,8 @@
-import { Model, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey } from 'sequelize';
+import { Model, InferAttributes, InferCreationAttributes, ForeignKey } from 'sequelize';
 import { User } from './User.js';
 import { Facility } from './Facility.js';
 
 export class FacilityManager extends Model<InferAttributes<FacilityManager>, InferCreationAttributes<FacilityManager>> {
-  declare id: CreationOptional<number>;
   declare userId: ForeignKey<User['id']>;
   declare facilityId: ForeignKey<Facility['id']>;
-  declare createdAt: CreationOptional<Date>;
-  declare updatedAt: CreationOptional<Date>;
 }
