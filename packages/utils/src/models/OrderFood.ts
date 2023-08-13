292d8019c2dd0ede7add7fd376ef71bd222aac85
VasiliyTemniy <flash_er@mail.ru>
2023-08-13 14:22:30 +0300

    add types for OrderFoodDT

--- packages/utils/src/models/OrderFood.ts
+++ packages/utils/src/models/OrderFood.ts
@@ -1,1 +1,37 @@
-export const dummyOrderFoods = 'dummy';
+import { OrderFoodData } from "@m-cafe-app/db-models";
+import { isNumber, isString } from "../types/typeParsers.js";
+import { hasOwnProperty, MapToDT, MapToUnknown } from "../types/helpers.js";
+import { FoodDTS, isFoodDTS } from "./Food.js";
+
+
+export type OrderFoodDT = Omit<MapToDT<OrderFoodData>, 'foodId' | 'orderId'>
+& {
+  food?: FoodDTS;
+};
+
+type OrderFoodDTFields = MapToUnknown<OrderFoodDT>;
+
+const hasOrderFoodDTFields = (obj: unknown): obj is OrderFoodDTFields =>
+  hasOwnProperty(obj, "id")
+  &&
+  hasOwnProperty(obj, "amount")
+  &&
+  hasOwnProperty(obj, "archivePrice")
+  &&
+  hasOwnProperty(obj, "archiveFoodName");
+
+export const isOrderFoodDT = (obj: unknown): obj is OrderFoodDT => {
+  if (!hasOrderFoodDTFields(obj)) return false;
+
+  if (
+    hasOwnProperty(obj, "food") && !isFoodDTS(obj.food)
+  ) return false;
+  
+  return isNumber(obj.id)
+  &&
+  isNumber(obj.amount)
+  &&
+  isNumber(obj.archivePrice)
+  &&
+  isString(obj.archiveFoodName);
+};
