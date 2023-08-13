ccc8780f51f42e6da536b3a5aed10d867262d87c
VasiliyTemniy <flash_er@mail.ru>
2023-08-13 14:21:52 +0300

    upd Order & OrderFood declarations

--- packages/db-models/src/models/Order.ts
+++ packages/db-models/src/models/Order.ts
@@ -1,13 +1,24 @@
-import { Model, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey } from 'sequelize';
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey, NonAttribute } from 'sequelize';
 import { User } from './User.js';
 import { Address } from './Address.js';
+import { PropertiesCreationOptional } from '../types/helpers.js';
+import { OrderFood } from './OrderFood.js';
 
 export class Order extends Model<InferAttributes<Order>, InferCreationAttributes<Order>> {
   declare id: CreationOptional<number>;
   declare userId: ForeignKey<User['id']>;
-  declare addressId: ForeignKey<Address['id']>;
+  declare addressId: ForeignKey<Address['id']> | null;
   declare deliverAt: Date;
   declare status: string;
+  declare totalCost: number;
+  declare archiveAddress: string;
   declare createdAt: CreationOptional<Date>;
   declare updatedAt: CreationOptional<Date>;
+  declare user?: NonAttribute<User>;
+  declare address?: NonAttribute<Address>;
+  declare orderFoods?: NonAttribute<OrderFood>;
 }
+
+
+export type OrderData = Omit<InferAttributes<Order>, PropertiesCreationOptional>
+  & { id: number; };
--- packages/db-models/src/models/OrderFood.ts
+++ packages/db-models/src/models/OrderFood.ts
@@ -1,11 +1,18 @@
-import { Model, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey } from 'sequelize';
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey, NonAttribute } from 'sequelize';
+import { PropertiesCreationOptional } from '../types/helpers.js';
 import { Food } from './Food.js';
 import { Order } from './Order.js';
 
 export class OrderFood extends Model<InferAttributes<OrderFood>, InferCreationAttributes<OrderFood>> {
   declare id: CreationOptional<number>;
   declare orderId: ForeignKey<Order['id']>;
-  declare foodId: ForeignKey<Food['id']>;
+  declare foodId: ForeignKey<Food['id']> | null;
   declare amount: number;
   declare archivePrice: number;
+  declare archiveFoodName: string;
+  declare food?: NonAttribute<Food>;
 }
+
+
+export type OrderFoodData = Omit<InferAttributes<OrderFood>, PropertiesCreationOptional>
+  & { id: number; };
