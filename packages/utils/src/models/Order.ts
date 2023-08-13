139f240ce6d459c8a262d6c62283943fec6dd6c3
VasiliyTemniy <flash_er@mail.ru>
2023-08-13 14:22:22 +0300

    add types for OrderDT

--- packages/utils/src/models/Order.ts
+++ packages/utils/src/models/Order.ts
@@ -1,1 +1,53 @@
-export const dummyOrders = 'dummy';
+import { OrderData } from "@m-cafe-app/db-models";
+import { isNumber, isString } from "../types/typeParsers.js";
+import { hasOwnProperty, MapToDT, MapToUnknown } from "../types/helpers.js";
+import { isUserDT, UserDT } from "./User.js";
+import { AddressDT, isAddressDT } from "./Address.js";
+import { isOrderFoodDT, OrderFoodDT } from "./OrderFood.js";
+
+
+export type OrderDT = Omit<MapToDT<OrderData>, 'userId' | 'addressId'>
+& {
+  user?: UserDT;
+  address?: AddressDT;
+  orderFoods: OrderFoodDT[];
+};
+
+type OrderDTFields = MapToUnknown<OrderDT>;
+
+const hasOrderDTFields = (obj: unknown): obj is OrderDTFields =>
+  hasOwnProperty(obj, "id")
+  &&
+  hasOwnProperty(obj, "deliverAt")
+  &&
+  hasOwnProperty(obj, "status")
+  &&
+  hasOwnProperty(obj, "totalCost")
+  &&
+  hasOwnProperty(obj, "orderFoods")
+  &&
+  hasOwnProperty(obj, "archiveAddress");
+
+export const isOrderDT = (obj: unknown): obj is OrderDT => {
+  if (!hasOrderDTFields(obj)) return false;
+
+  if (
+    (hasOwnProperty(obj, "address") && !isAddressDT(obj.address))
+    ||
+    (hasOwnProperty(obj, "user") && !isUserDT(obj.user))
+  ) return false;
+
+  if (!Array.isArray(obj.orderFoods)) return false;
+  for (const orderFood of obj.orderFoods)
+    if (!isOrderFoodDT(orderFood)) return false;
+
+  return isNumber(obj.id)
+  &&
+  isString(obj.deliverAt)
+  &&
+  isString(obj.status)
+  &&
+  isNumber(obj.totalCost)
+  &&
+  isString(obj.archiveAddress);
+};
