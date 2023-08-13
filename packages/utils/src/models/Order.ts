ed7f89fddc304cd186e4805826d4f84c020ae2e0
VasiliyTemniy <flash_er@mail.ru>
2023-08-13 19:24:06 +0300

    add OrderDTS; add Facility to Order

--- packages/utils/src/models/Order.ts
+++ packages/utils/src/models/Order.ts
@@ -4,13 +4,15 @@ import { hasOwnProperty, MapToDT, MapToUnknown } from "../types/helpers.js";
 import { isUserDT, UserDT } from "./User.js";
 import { AddressDT, isAddressDT } from "./Address.js";
 import { isOrderFoodDT, OrderFoodDT } from "./OrderFood.js";
+import { FacilityDTS, isFacilityDTS } from "./Facility.js";
 
 
-export type OrderDT = Omit<MapToDT<OrderData>, 'userId' | 'addressId'>
+export type OrderDT = Omit<MapToDT<OrderData>, 'userId' | 'addressId' | 'facilityId'>
 & {
   user?: UserDT;
   address?: AddressDT;
   orderFoods: OrderFoodDT[];
+  facility: FacilityDTS;
 };
 
 type OrderDTFields = MapToUnknown<OrderDT>;
@@ -28,7 +30,9 @@ const hasOrderDTFields = (obj: unknown): obj is OrderDTFields =>
   &&
   hasOwnProperty(obj, "archiveAddress")
   &&
-  hasOwnProperty(obj, "customerPhonenumber");
+  hasOwnProperty(obj, "customerPhonenumber")
+  &&
+  hasOwnProperty(obj, "facility");
 
 export const isOrderDT = (obj: unknown): obj is OrderDT => {
   if (!hasOrderDTFields(obj)) return false;
@@ -55,5 +59,52 @@ export const isOrderDT = (obj: unknown): obj is OrderDT => {
   &&
   isString(obj.archiveAddress)
   &&
-  isString(obj.customerPhonenumber);
+  isString(obj.customerPhonenumber)
+  &&
+  isFacilityDTS(obj.facility);
+};
+
+
+export type OrderDTS = Omit<OrderDT, 'user' | 'orderFoods' | 'address' | 'facility'>
+& {
+  facilityId: number;
+};
+
+type OrderDTSFields = MapToUnknown<OrderDTS>;
+
+const hasOrderDTSFields = (obj: unknown): obj is OrderDTSFields =>
+  hasOwnProperty(obj, "id")
+  &&
+  hasOwnProperty(obj, "deliverAt")
+  &&
+  hasOwnProperty(obj, "status")
+  &&
+  hasOwnProperty(obj, "totalCost")
+  &&
+  hasOwnProperty(obj, "archiveAddress")
+  &&
+  hasOwnProperty(obj, "customerPhonenumber")
+  &&
+  hasOwnProperty(obj, "facilityId");
+
+export const isOrderDTS = (obj: unknown): obj is OrderDTS => {
+  if (!hasOrderDTSFields(obj)) return false;
+
+  if (
+    (hasOwnProperty(obj, "customerName") && !isString(obj.customerName))
+  ) return false;
+
+  return isNumber(obj.id)
+  &&
+  isString(obj.deliverAt)
+  &&
+  isString(obj.status)
+  &&
+  isNumber(obj.totalCost)
+  &&
+  isString(obj.archiveAddress)
+  &&
+  isString(obj.customerPhonenumber)
+  &&
+  isNumber(obj.facilityId);
 };
