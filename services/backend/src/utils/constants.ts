8f2c97638123e9bf52bf10e9f10efe452a43db3c
VasiliyTemniy <flash_er@mail.ru>
2023-08-16 15:42:26 +0300

    add possible user rights; add delayedStatusMS

--- services/backend/src/utils/constants.ts
+++ services/backend/src/utils/constants.ts
@@ -153,3 +153,19 @@ export const minEntranceKeyLen = process.env.ENTRANCE_KEY_MINLEN
 export const maxEntranceKeyLen = process.env.ENTRANCE_KEY_MAXLEN
   ? Number(process.env.ENTRANCE_KEY_MAXLEN)
   : 20;
+
+
+export const possibleUserRights = ['user', 'manager', 'admin', 'disabled'];
+
+
+export const delayedStatusMS = {
+  accepted: process.env.DELAYED_STATUS_MS_ACCEPTED
+    ? Number(process.env.DELAYED_STATUS_MS_ACCEPTED)
+    : 50 * 60 * 1000, 
+  cooking: process.env.DELAYED_STATUS_MS_COOKING
+    ? Number(process.env.DELAYED_STATUS_MS_COOKING)
+    : 30 * 60 * 1000,
+  delivering: process.env.DELAYED_STATUS_MS_DELIVERING
+    ? Number(process.env.DELAYED_STATUS_MS_DELIVERING)
+    : 5 * 60 * 1000,
+};
