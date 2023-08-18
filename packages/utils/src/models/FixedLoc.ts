b9815f32a6d8f209dc1a100af99a6edc4badfa2d
VasiliyTemniy <flash_er@mail.ru>
2023-08-18 20:40:16 +0300

    add FixedLoc model + types

---
+++ packages/utils/src/models/FixedLoc.ts
@@ -0,0 +1,18 @@
+import { FixedLocData } from "@m-cafe-app/db-models";
+import { isNumber, isString } from "../types/typeParsers.js";
+import { hasOwnProperty, MapToDT, MapToUnknown } from "../types/helpers.js";
+import { isLocStringDT, LocStringDT } from "./LocString.js";
+
+
+export type FixedLocDT = Omit<MapToDT<FixedLocData>, 'locStringId'>
+& {
+  locString: LocStringDT;
+};
+
+type FixedLocDTFields = MapToUnknown<FixedLocDT>;
+
+const hasFixedLocDTFields = (obj: unknown): obj is FixedLocDTFields =>
+  hasOwnProperty(obj, "id") && hasOwnProperty(obj, "name") && hasOwnProperty(obj, "locString");
+
+export const isFixedLocDT = (obj: unknown): obj is FixedLocDT =>
+  hasFixedLocDTFields(obj) && isNumber(obj.id) && isString(obj.name) && isLocStringDT(obj.locString);
