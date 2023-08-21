042b173c0eef53c648ea853c3a4f65b9a2fe60a6
VasiliyTemniy <flash_er@mail.ru>
2023-08-21 22:53:23 +0300

    split frontend to services: user, admin, manager

--- services/frontend/webpack_config/webpack.plugins.ts
+++ services/frontend/webpack_config/webpack.plugins.ts
@@ -1,5 +1,4 @@
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import HtmlWebPackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import webpack from "webpack";
@@ -9,10 +8,6 @@ export const plugins = [
     logger: 'webpack-infrastructure',
   }),
   new CleanWebpackPlugin(),
-  new HtmlWebPackPlugin({
-    template: './public/index.html',
-    filename: './index.html'
-  }),
   new MiniCssExtractPlugin({
     filename: '[name].[fullhash].css',
     chunkFilename: '[id].[fullhash].css'
