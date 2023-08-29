630b81672d7e280185d04d2923fa17c9239c9e45
VasiliyTemniy <flash_er@mail.ru>
2023-08-29 22:05:43 +0300

    upd reexport

--- packages/frontend-logic/src/shared/index.ts
+++ packages/frontend-logic/src/shared/index.ts
@@ -1,2 +1,9 @@
+import { frontendModule } from '../utils/config';
+
 export { sendLogin, sendNewUser } from './reducers/userReducer';
 export { useTranslation } from './hooks/useTranslation';
+
+export const { useAppDispatch, useAppSelector } =
+  frontendModule === 'admin' ? await import('../../src/admin') :
+  frontendModule === 'manager' ? await import('../../src/manager') :
+  await import('../../src/customer');
