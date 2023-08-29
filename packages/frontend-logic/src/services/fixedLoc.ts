7700d72077c8716815d0af2c2e79ead6e7b6bbd2
VasiliyTemniy <flash_er@mail.ru>
2023-08-29 14:51:20 +0300

    add update many route; specify reqBodies

--- packages/frontend-logic/src/services/fixedLoc.ts
+++ packages/frontend-logic/src/services/fixedLoc.ts
@@ -1,42 +1,70 @@
 import axios from 'axios';
-import { NewFixedLocBody, EditFixedLocBody } from '@m-cafe-app/utils';
+import { NewFixedLocBody, EditFixedLocBody, FixedLocDT, EditManyFixedLocBody } from '@m-cafe-app/utils';
 import { RequestOptions } from '../types/requestOptions';
 
+/**
+ * Should not ever be used. FixedLocs are fixed, only updatable. Delete this?
+ */
 const createLoc = async (newLoc: NewFixedLocBody, options: RequestOptions) => {
 
-  const config = { headers: {'Content-Type': 'application/json'} };
+  const reqBody: NewFixedLocBody = newLoc;
+
+  const config = { headers: {'Content-Type': 'application/json'}, withCredentials: true };
   const { data: fixedLoc } = await axios.post<JSON>(
     `${options.apiBaseUrl}/api/fixed-loc`,
-    JSON.stringify(newLoc),
+    JSON.stringify(reqBody),
     config
   );
 
   return fixedLoc;
 };
 
+/**
+ * Not recommended, better to update many at once
+ */
 const updateLoc = async (updLoc: EditFixedLocBody, locId: number, options: RequestOptions) => {
+
+  const reqBody: EditFixedLocBody = updLoc;
 
   const config = { headers: {'Content-Type': 'application/json'}, withCredentials: true };
   const { data: fixedLoc } = await axios.put<JSON>(
     `${options.apiBaseUrl}/api/fixed-loc/${locId}`,
-    JSON.stringify(updLoc),
+    JSON.stringify(reqBody),
     config
   );
 
   return fixedLoc;
 };
 
-const getLocs = async (options: RequestOptions) => {
+/**
+ * Recommended to update fixed locs
+ */
+const updateManyLocs = async (updLocs: FixedLocDT[], options: RequestOptions) => {
 
-  const config = { withCredentials: true };
-  const { data: fixedLocs } = await axios.get<JSON>(
-    `${options.apiBaseUrl}/api/fixed-loc`,
+  const reqBody: EditManyFixedLocBody = { updLocs };
+
+  const config = { headers: {'Content-Type': 'application/json'}, withCredentials: true };
+  const { data: fixedLocs } = await axios.put<JSON>(
+    `${options.apiBaseUrl}/api/fixed-loc/all`,
+    JSON.stringify(reqBody),
     config
   );
 
   return fixedLocs;
 };
 
+const getLocs = async (options: RequestOptions) => {
+
+  const { data: fixedLocs } = await axios.get<JSON>(
+    `${options.apiBaseUrl}/api/fixed-loc`
+  );
+
+  return fixedLocs;
+};
+
+/**
+ * Should not ever be used? Delete this?
+ */
 const deleteLoc = async (locId: number, options: RequestOptions) => {
 
   const config = { withCredentials: true };
@@ -47,4 +75,4 @@ const deleteLoc = async (locId: number, options: RequestOptions) => {
 
 };
 
-export default { createLoc, updateLoc, getLocs, deleteLoc };
+export default { createLoc, updateLoc, updateManyLocs, getLocs, deleteLoc };
