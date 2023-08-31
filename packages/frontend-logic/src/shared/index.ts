610180fae997d17cfebb6b79333a2f1cbf447d3d
VasiliyTemniy <flash_er@mail.ru>
2023-08-31 22:29:06 +0300

    rework reexports

--- packages/frontend-logic/src/shared/index.ts
+++ packages/frontend-logic/src/shared/index.ts
@@ -1,28 +1,6 @@
 import { frontendModule } from '../utils/config';
 
-/**
- * Reducers section begin
- */
-
-export { sendLogin, sendNewUser } from './reducers/userReducer';
-export { useTranslation } from './hooks/useTranslation';
-
-/**
- * Reducers section end
- */
-
 export const { useAppDispatch, useAppSelector } =
   frontendModule === 'admin' ? await import('../../src/admin') :
   frontendModule === 'manager' ? await import('../../src/manager') :
   await import('../../src/customer');
-
-/**
- * Components section begin
- */
-
-export * from './components/LoginPage';
-export * from './components/FormikFields';
-
-/**
- * Components section end
- */
