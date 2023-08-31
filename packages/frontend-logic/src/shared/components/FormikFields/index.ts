9e8ec214f8190146e54aebad6829fc2de374edab
VasiliyTemniy <flash_er@mail.ru>
2023-08-31 22:31:46 +0300

    add index for formik fields

--- packages/frontend-logic/src/shared/components/FormikFields/index.ts
+++ packages/frontend-logic/src/shared/components/FormikFields/index.ts
@@ -1,1 +1,5 @@
-export * from './FormikFieldsSC';
+export * from './FormikDateFieldSC';
+export * from './FormikSelectFieldSC';
+export * from './FormikTextAreaFieldSC';
+export * from './FormikTextFieldSC';
+export * from './FormikTimeFieldSC';
