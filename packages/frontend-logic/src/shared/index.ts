7758cf0a7fabd4f01707a337711363dcbea3f638
VasiliyTemniy <flash_er@mail.ru>
2023-09-01 17:50:24 +0300

    specify filename

--- packages/frontend-logic/src/shared/index.ts
+++
@@ -1,6 +0,0 @@
-import { frontendModule } from '../utils/config';
-
-export const { useAppDispatch, useAppSelector } =
-  frontendModule === 'admin' ? await import('../../src/admin') :
-  frontendModule === 'manager' ? await import('../../src/manager') :
-  await import('../../src/customer');
