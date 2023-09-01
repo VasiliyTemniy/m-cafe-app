f49efaad0230332beff9f2b8da15ca480b96d039
VasiliyTemniy <flash_er@mail.ru>
2023-09-01 17:54:58 +0300

    add all needed envs for compile-time

--- services/frontend/webpack_config/webpack.plugins.ts
+++ services/frontend/webpack_config/webpack.plugins.ts
@@ -3,6 +3,8 @@ import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import webpack from "webpack";
 
+const frontendModule = process.env.FRONTEND_MODULE ? process.env.FRONTEND_MODULE : 'customer';
+
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
@@ -13,5 +15,55 @@ export const plugins = [
     chunkFilename: '[id].[fullhash].css'
   }),
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  new webpack.EnvironmentPlugin(['NODE_ENV', 'BACKEND_URL'])
+  new webpack.EnvironmentPlugin({
+    NODE_ENV: 'production',
+    BACKEND_URL: 'must_be_provided',
+    FRONTEND_MODULE: frontendModule,
+    FRONTEND_MODULE_ADMIN: frontendModule === 'admin',
+    FRONTEND_MODULE_MANAGER: frontendModule === 'manager',
+    FRONTEND_MODULE_CUSTOMER: frontendModule === 'customer',
+    FRONTEND_TARGET_WEB: true,
+    USERNAME_REGEXP: '',
+    USERNAME_MINLEN: '',
+    USERNAME_MAXLEN: '',
+    NAME_REGEXP: '',
+    NAME_MINLEN: '',
+    NAME_MAXLEN: '',
+    PHONENUMBER_REGEXP: '',
+    PHONENUMBER_MINLEN: '',
+    PHONENUMBER_MAXLEN: '',
+    EMAIL_REGEXP: '',
+    EMAIL_MINLEN: '',
+    EMAIL_MAXLEN: '',
+    DATE_REGEXP: '',
+    PASSWORD_REGEXP: '',
+    PASSWORD_MINLEN: '',
+    PASSWORD_MAXLEN: '',
+    REGION_REGEXP: '',
+    REGION_MINLEN: '',
+    REGION_MAXLEN: '',
+    DISTRICT_REGEXP: '',
+    DISTRICT_MINLEN: '',
+    DISTRICT_MAXLEN: '',
+    CITY_REGEXP: '',
+    CITY_MINLEN: '',
+    CITY_MAXLEN: '',
+    STREET_REGEXP: '',
+    STREET_MINLEN: '',
+    STREET_MAXLEN: '',
+    HOUSE_REGEXP: '',
+    HOUSE_MINLEN: '',
+    HOUSE_MAXLEN: '',
+    ENTRANCE_REGEXP: '',
+    ENTRANCE_MINLEN: '',
+    ENTRANCE_MAXLEN: '',
+    FLOOR_MINLEN: '',
+    FLOOR_MAXLEN: '',
+    FLAT_REGEXP: '',
+    FLAT_MINLEN: '',
+    FLAT_MAXLEN: '',
+    ENTRANCE_KEY_REGEXP: '',
+    ENTRANCE_KEY_MINLEN: '',
+    ENTRANCE_KEY_MAXLEN: ''
+  })
 ];
