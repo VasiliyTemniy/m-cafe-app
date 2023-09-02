6143e0b95e59868d9a79032e8ed91e732448332e
VasiliyTemniy <flash_er@mail.ru>
2023-09-02 22:39:51 +0300

    upd CommonProps

--- packages/frontend-logic/types/CommonProps.ts
+++ packages/frontend-logic/types/CommonProps.ts
@@ -1,44 +1,22 @@
-import {
-  ChangeEventHandler,
-  FocusEventHandler,
-  CSSProperties
-} from "react";
-
 export type InputSpecificValue = {
   labelAsPlaceholder: boolean,
-  labelText: string,
   useBarBelow: boolean
 };
 
 export type LCSpecificValue = undefined | InputSpecificValue;
 
-export interface CommonSCProps {
+export interface CommonProps {
   classNameOverride?: string;
   classNameAddon?: string;
+  className?: string;
   id?: string;
-  tooltipTNode?: string;
   tooltip?: string;
 }
 
-export interface CommonLCProps {
-  id?: string;
-  className?: string;
-  style?: CSSProperties;
-  specific?: LCSpecificValue;
-}
-
-export interface CommonFieldSCProps extends CommonSCProps {
+export interface CommonFieldProps extends CommonProps {
   placeholder?: string;
   label?: string;
-}
-
-export interface CommonFieldLCProps extends CommonLCProps {
-  placeholder?: string;
-  label?: string;
-  name: string;
-  value: string;
-  onChange: ChangeEventHandler<HTMLInputElement>;
-  onBlur: FocusEventHandler<HTMLInputElement>;
-  errorMessage?: string;
+  // onChange: ChangeEventHandler<HTMLInputElement>;
+  // onBlur: FocusEventHandler<HTMLInputElement>;
   disabled?: boolean;
 }
