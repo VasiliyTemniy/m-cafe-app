a01dd73518946bdbb11aa1e2eb3a2606c1f058a3
VasiliyTemniy <flash_er@mail.ru>
2023-09-02 22:40:47 +0300

    minor

--- packages/shared-constants/src/inlineCSSProperties.ts
+++ packages/shared-constants/src/inlineCSSProperties.ts
@@ -1,13 +1,17 @@
 // Maybe will be updated
-const possibleCSSPropertiesKeys = [
+const allowedCSSPropertiesKeys = [
   'backgroundColor',
   'borderRadius'
 ] as const;
 
-export type PossibleCSSPropertiesKeys = typeof possibleCSSPropertiesKeys[number];
+export type AllowedCSSPropertiesKeys = typeof allowedCSSPropertiesKeys[number];
 
-const possibleCSSPropertiesKeysSet = new Set([ ...possibleCSSPropertiesKeys as readonly string[] ]);
+export type AllowedCSSProperties = {
+  [key in AllowedCSSPropertiesKeys]: string | number;
+};
 
-export const isCSSPropertyKey = (key: string): key is PossibleCSSPropertiesKeys => {
-  return possibleCSSPropertiesKeysSet.has(key);
+const allowedCSSPropertiesKeysSet = new Set([ ...allowedCSSPropertiesKeys as readonly string[] ]);
+
+export const isCSSPropertyKey = (key: string): key is AllowedCSSPropertiesKeys => {
+  return allowedCSSPropertiesKeysSet.has(key);
 };
