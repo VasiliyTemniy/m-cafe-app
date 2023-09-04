35f6682110ba73f9a70930ff311440bdef538be4
VasiliyTemniy <flash_er@mail.ru>
2023-09-04 12:02:55 +0300

    make initUiSettings read jsonc files with initial ui settings

--- services/backend/src/utils/initUiSettings.ts
+++ services/backend/src/utils/initUiSettings.ts
@@ -1,34 +1,107 @@
-import { UiSettingData, UiSetting } from '@m-cafe-app/db';
+import { UiSetting } from '@m-cafe-app/db';
+import { ApplicationError, isString } from '@m-cafe-app/utils';
+import logger from './logger.js';
+import { getFileReadPromises } from './getFileReadPromises.js';
+import { allowedThemes } from '@m-cafe-app/shared-constants';
 
+/**
+ * Look for all jsonc files in initialUiSettings folder
+ * 
+ * Inject theme after componentType/componentName
+ * 
+ * Add to DB if not exists
+ */
 export const initUiSettings = async () => {
 
-  const uiSettings = [] as UiSetting[];
+  try {
+    const fileReadResults = await getFileReadPromises('initialUiSettings', 'jsonc');
 
-  for (const uiSetting of initialUiSettings) {
-    // No value update if found!
-    const [savedUiSetting, _created] = await UiSetting.findOrCreate({
-      where: { name: uiSetting.name },
-      defaults: { name: uiSetting.name, value: uiSetting.value }
-    });
-    uiSettings.push(savedUiSetting);
+    for (const fileReadResult of fileReadResults) {
+
+      // Strip JSONC comments
+      const strippedFromCommentsFileReadResult = fileReadResult.replace(/\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g, (m, g) => g ? "" : m);
+
+      const uiSettingsTree = JSON.parse(strippedFromCommentsFileReadResult) as JSON;
+      await parseUiSettingsTree(uiSettingsTree);
+    }
+
+  } catch (error) {
+    logger.error(error);
   }
+};
 
-  return uiSettings;
+const parseUiSettingsTree = async (uiSettingsTree: JSON) => {
+
+  for (const key in uiSettingsTree) {
+    const uiNodeObj = uiSettingsTree[key as keyof JSON];
+    if (!isUiNodeObj(uiNodeObj)) throw new ApplicationError('Wrong ui settings structure! Check ui settings', { current: uiSettingsTree });
+    await parseUiNodeTree(uiNodeObj, key);
+  }
 
 };
 
-// Will be changed and updated
-const initialUiSettings: Omit<UiSettingData, 'id'>[] = [
-  {
-    name: 'containerShape',
-    value: 'rightAngled'
-  },
-  {
-    name: 'buttonStyle',
-    value: 'alpha'
-  },
-  {
-    name: 'inputStyle',
-    value: 'alpha'
-  },
-];
+type UiNodeObj = {
+  [key:string] : unknown
+};
+
+const isUiNodeObj = (obj: unknown): obj is UiNodeObj => {
+  if (!obj || !(typeof obj === 'object')) return false;
+  for (const key in obj) {
+    if (!isString(key)) return false;
+  }
+  return true;
+};
+
+const parseUiNodeTree = async (uiNodeParent: UiNodeObj, uiNodePath: string) => {
+
+  for (const key in uiNodeParent) {
+
+    // If found value then append foundUiSettings
+    if (key === 'value') {
+      if (!isString(uiNodeParent[key])) throw new ApplicationError('Wrong ui settings structure! Check ui settings', { current: uiNodeParent });
+      await addUiSettingToDB(uiNodePath, uiNodeParent[key] as string);
+      // Break here means continue for parent of uiNodeParent
+      break;
+    } else {
+      const tNodeChild = uiNodeParent[key];
+      if (!isUiNodeObj(tNodeChild)) throw new ApplicationError('Wrong JSON format! Check ui settings', { current: uiNodeParent });
+      await parseUiNodeTree(tNodeChild, uiNodePath + '.' + key);
+    }
+  }
+};
+
+const addUiSettingToDB = async (uiNodePath: string, value: string) => {
+
+  // Apply initial values to all themes
+  for (const theme of allowedThemes) {
+
+    let uiSettingName: string = '';
+
+    const uiNodes = uiNodePath.split('.');
+
+    // Inject theme to upperUiNode
+    const upperUiNode = uiNodes[0];
+    const upperUiNodeParts = upperUiNode.split('-');
+    let themedUpperUiNode: string = upperUiNodeParts[0] + '-' + theme;
+    for (let i = 1; i < upperUiNodeParts.length; i++) {
+      themedUpperUiNode = themedUpperUiNode + '-' + upperUiNodeParts[i];
+    }
+
+    uiSettingName = themedUpperUiNode;
+
+    // Reassemble nodePath with injected theme
+    for (let i = 1; i < uiNodes.length; i++) {
+      uiSettingName = uiSettingName + '.' + uiNodes[i];
+    }
+
+    const foundUiSetting = await UiSetting.findOne({ where: { name: uiSettingName }});
+
+    if (!foundUiSetting) {
+      await UiSetting.create({
+        name: uiSettingName,
+        value
+      });
+    }
+  }
+
+};
