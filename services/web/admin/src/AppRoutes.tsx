4a856ce355f9ab18c23b0f6dc30a0c1ce10f6e9c
VasiliyTemniy <flash_er@mail.ru>
2023-09-04 12:04:17 +0300

    upd

--- services/web/admin/src/AppRoutes.tsx
+++ services/web/admin/src/AppRoutes.tsx
@@ -1,19 +1,17 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
 
-import { UnderConstructionLC } from '../../shared/components';
+import { UnderConstruction } from 'shared/components';
 
 import { apiBaseUrl } from '@m-cafe-app/shared-constants';
 
-const AppRoutes = () => {
+export const AppRoutes = () => {
   return (
     <>
       <Outlet />
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/under-construction" element={<UnderConstructionLC svgUrl={`${apiBaseUrl}/public/pictures/svg/construction.svg`}/>} />
+        <Route path="/" element={<UnderConstruction svgUrl={`${apiBaseUrl}/public/pictures/svg/construction.svg`}/>} />
+        <Route path="/under-construction" element={<UnderConstruction svgUrl={`${apiBaseUrl}/public/pictures/svg/construction.svg`}/>} />
       </Routes>
     </>
   );
 };
-
-export default AppRoutes;
