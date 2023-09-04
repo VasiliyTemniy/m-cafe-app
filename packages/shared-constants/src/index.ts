fcdd0c5a575cb0808b3b762cb8a13858a5cd9fe7
VasiliyTemniy <flash_er@mail.ru>
2023-09-04 12:00:31 +0300

    upd index

--- packages/shared-constants/src/index.ts
+++ packages/shared-constants/src/index.ts
@@ -2,3 +2,4 @@ export * from './validationConstants.js';
 export * from './inlineCSSProperties.js';
 export * from './possibleUserRights.js';
 export * from './apiBaseUrl.js';
+export * from './allowedThemes.js';
