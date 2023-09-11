9f34d21bf9c7553bba32c3d3e2642e53cd3c50d7
VasiliyTemniy <flash_er@mail.ru>
2023-09-11 22:53:25 +0300

    fix pr

--- services/web/admin/src/AppRoutes.tsx
+++ services/web/admin/src/AppRoutes.tsx
@@ -3,14 +3,13 @@ import { Outlet, Route, Routes } from 'react-router-dom';
 import { UnderConstruction } from 'shared/components';
 
 import { apiBaseUrl } from '@m-cafe-app/shared-constants';
-import { ColorTestPage } from 'shared/components/ColorTestPage';
 
 export const AppRoutes = () => {
   return (
     <>
       <Outlet />
       <Routes>
-        <Route path="/" element={<ColorTestPage/>} />
+        <Route path="/" element={<UnderConstruction svgUrl={`${apiBaseUrl}/public/pictures/svg/construction.svg`}/>} />
         <Route path="/under-construction" element={<UnderConstruction svgUrl={`${apiBaseUrl}/public/pictures/svg/construction.svg`}/>} />
       </Routes>
     </>
