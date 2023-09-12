c55bb2b065204a6016504de7d52ef45777f277b2
VasiliyTemniy <flash_er@mail.ru>
2023-09-12 14:07:09 +0300

    upd

--- services/web/admin/src/App.tsx
+++ services/web/admin/src/App.tsx
@@ -1,6 +1,6 @@
 import { useAppSelector, useInitAppAdmin } from '@m-cafe-app/frontend-logic/admin/hooks';
 import { AppRoutes } from './AppRoutes';
-import { LoginPage, Loading, Scrollable, Container } from 'shared/components';
+import { LoginPage, Loading, Scrollable, Container, Header } from 'shared/components';
 
 
 export const App = () => {
@@ -17,9 +17,9 @@ export const App = () => {
   if (!user.phonenumber) {
     return (
       <>
-        {/* <Header /> */}
         {/* <Notification/> */}
         <Container id='app-wrapper'>
+          <Header />
           <Scrollable wrapperClassNameAddon='app-content-wrapper' id='app-content'>
             {/* <ColorTestPage/> */}
             <LoginPage/>
