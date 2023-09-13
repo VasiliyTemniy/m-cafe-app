d8fd0ff238f9f608cf827bb2f26378c1ff1cf539
VasiliyTemniy <flash_er@mail.ru>
2023-09-13 22:34:49 +0300

    TextComp rework

--- services/web/shared/components/basic/TextComp.tsx
+++ services/web/shared/components/basic/TextComp.tsx
@@ -4,6 +4,7 @@ import { useInitLC } from '@m-cafe-app/frontend-logic/shared/hooks';
 interface TextCompProps extends CommonProps {
   text?: string;
   children?: JSX.Element[] | JSX.Element;
+  htmlEl?: 'div' | 'span' | 'p' | 'h1' | 'h2' | 'h3' | 'h4';
 }
 
 export const TextComp = ({
@@ -11,24 +12,94 @@ export const TextComp = ({
   classNameAddon,
   id,
   text,
-  children
+  children,
+  htmlEl = 'div'
 }: TextCompProps) => {
 
   const { className, style } = useInitLC({
-    componentType: 'container',
+    componentType: 'text',
     componentName: 'text',
     classNameAddon,
     classNameOverride,
   });
 
-  return (
-    <div
-      className={className}
-      id={id}
-      style={style}
-    >
-      {text}
-      {children}
-    </div>
-  );
+  switch (htmlEl) {
+    case 'div':
+      return (
+        <div
+          className={className}
+          id={id}
+          style={style}
+        >
+          {text}
+          {children}
+        </div>
+      );
+    case 'span':
+      return (
+        <span
+          className={className}
+          id={id}
+          style={style}
+        >
+          {text}
+          {children}
+        </span>
+      );
+    case 'p':
+      return (
+        <p
+          className={className}
+          id={id}
+          style={style}
+        >
+          {text}
+          {children}
+        </p>
+      );
+    case 'h1':
+      return (
+        <h1
+          className={className}
+          id={id}
+          style={style}
+        >
+          {text}
+          {children}
+        </h1>
+      );
+    case 'h2':
+      return (
+        <h2
+          className={className}
+          id={id}
+          style={style}
+        >
+          {text}
+          {children}
+        </h2>
+      );
+    case 'h3':
+      return (
+        <h3
+          className={`${className} w100`}
+          id={id}
+          style={style}
+        >
+          {text}
+          {children}
+        </h3>
+      );
+    case 'h4':
+      return (
+        <h4
+          className={className}
+          id={id}
+          style={style}
+        >
+          {text}
+          {children}
+        </h4>
+      );
+  }
 };
