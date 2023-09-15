01916137bb6bc71c13dc164c40b277c500615e93
VasiliyTemniy <flash_er@mail.ru>
2023-09-15 22:38:14 +0300

    total fix for Modal appearence

--- services/web/shared/components/basic/Modal.tsx
+++ services/web/shared/components/basic/Modal.tsx
@@ -1,13 +1,17 @@
 import type { CommonProps } from '@m-cafe-app/frontend-logic/types';
 import { useInitLC } from '@m-cafe-app/frontend-logic/shared/hooks';
 import { TextComp } from './TextComp';
+import { useRef, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Scrollable } from './Scrollable';
+
 
 interface ModalProps extends CommonProps {
   children: JSX.Element[] | JSX.Element;
   title: string;
   subtitle?: string;
   active: boolean;
-  wrapper?: boolean;
+  withBlur?: boolean;
 }
 
 export const Modal = ({
@@ -18,8 +22,14 @@ export const Modal = ({
   title,
   subtitle,
   active,
-  wrapper = true
+  withBlur = true
 }: ModalProps) => {
+
+  const modalRef = useRef<HTMLDivElement>(null);
+  const observer = useRef<ResizeObserver | null>(null);
+  const [wrapperHeight, setWrapperHeight] = useState(0);
+  const [wrapperTop, setWrapperTop] = useState(0);
+  const [modalTop, setModalTop] = useState(10);
 
   const { className, style } = useInitLC({
     componentType: 'modal',
@@ -28,10 +38,44 @@ export const Modal = ({
     classNameOverride,
   });
 
-  if (active && wrapper) {
-    return (
-      <div className='modal-blur-wrapper'>
-        <div className={className} style={style} id={id}>
+  const handleResize = (parent: HTMLElement, modal: HTMLDivElement, header: HTMLElement) => {
+    const newWrapperHeight = (parent.clientHeight - header.clientHeight) < modal.clientHeight
+      ? parent.scrollHeight - header.clientHeight
+      : parent.clientHeight - header.clientHeight;
+    setWrapperHeight(newWrapperHeight);
+    setWrapperTop(header.clientHeight);
+    const newModalTop = Math.max((newWrapperHeight - modal.clientHeight) / 2 , 0);
+    setModalTop(newModalTop);
+  };
+
+  useEffect(() => {
+    const header = document.getElementById('app-header');
+    if (modalRef.current && header) {
+      const parent = document.body;
+      const modal = modalRef.current;
+      observer.current = new ResizeObserver(() => {
+        handleResize(parent, modal, header);
+      });
+      observer.current.observe(parent);
+      return () => {
+        observer.current?.unobserve(parent);
+      };
+    }
+  }, []);
+
+  if (active) {
+    return ReactDOM.createPortal(
+      <Scrollable
+        wrapperClassNameAddon={`modal-wrapper${withBlur ? ' blur' : ''}`}
+        wrapperStyle={{ height: `${wrapperHeight}px`, top: `${wrapperTop}px` }}
+        highlightScrollbarOnContentHover={false}
+      >
+        <div
+          className={className}
+          ref={modalRef}
+          style={{ ...style, top: `${modalTop}px` }}
+          id={id}
+        >
           <div className='modal-title'>
             <TextComp text={title} classNameAddon='title' htmlEl='h3'/>
             <TextComp text={subtitle} classNameAddon='subtitle' htmlEl='h4'/>
@@ -40,19 +84,8 @@ export const Modal = ({
             {children}
           </div>
         </div>
-      </div>
-    );
-  } else if (active) {
-    return (
-      <div className={className} style={style} id={id}>
-        <div className='modal-title'>
-          <TextComp text={title} classNameAddon='title' htmlEl='h3'/>
-          <TextComp text={subtitle} classNameAddon='subtitle' htmlEl='h4'/>
-        </div>
-        <div className='modal-main'>
-          {children}
-        </div>
-      </div>
+      </Scrollable>
+      , document.body
     );
   } else {
     return null;
