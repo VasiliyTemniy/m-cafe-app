7827b027455d405be7fa339e290d2b4e0d47f7db
VasiliyTemniy <flash_er@mail.ru>
2023-09-25 22:07:02 +0300

    add some placeholder routes

--- services/web/admin/src/router.tsx
+++ services/web/admin/src/router.tsx
@@ -1,14 +1,16 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import { UnderConstruction } from 'shared/components';
+import { ErrorPage, UnderConstruction } from 'shared/components';
 import { apiBaseUrl } from '@m-cafe-app/shared-constants';
 import { AppLayout } from './AppLayout';
 
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<AppLayout />}>
-      <Route index element={<div> Fuck dis sheet </div>} />
+    <Route element={<AppLayout />} errorElement={<ErrorPage/>}>
+      <Route index element={<div> Customer view will be here </div>} />
       <Route path="under-construction" element={<UnderConstruction svgUrl={`${apiBaseUrl}/public/pictures/svg/construction.svg`}/>} />
+      <Route path="fixed-locs" element={<div> fixed locs view </div>} />
+      <Route path="ui-settings" element={<div> ui settings view </div>} />
     </Route>
   ),
   {
