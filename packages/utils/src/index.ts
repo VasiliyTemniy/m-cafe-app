5db8b9a2ebd516746cad03bb2df98ffbf3eba3cd
VasiliyTemniy <flash_er@mail.ru>
2023-10-02 13:28:35 +0300

    rename typeParsers -> typeValidators

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,6 @@
 export * from './types/Errors.js';
 export * from './types/requestBodies/index.js';
 export * from './types/responseBodies/index.js';
-export * from './types/typeParsers.js';
+export * from './types/typeValidators.js';
 export * from './types/helpers.js';
 export * from './models/index.js';
