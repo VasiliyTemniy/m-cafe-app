28431bc9efea3681ad80f7bb5faaa99386173bc6
VasiliyTemniy <flash_er@mail.ru>
2023-10-02 13:20:48 +0300

    add namespace, scope to fixed_locs table

--- packages/db/src/migrations/17_fixed_locs.ts
+++ packages/db/src/migrations/17_fixed_locs.ts
@@ -1,4 +1,5 @@
 import type { MigrationContext } from '../types/MigrationContext.js';
+import { fixedLocsScopes } from '@m-cafe-app/shared-constants';
 import { DataTypes } from 'sequelize';
 
 export const up = async ({ context: queryInterface }: MigrationContext) => {
@@ -11,7 +12,20 @@ export const up = async ({ context: queryInterface }: MigrationContext) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: 'unique_fixed_loc'
+    },
+    namespace: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: 'unique_fixed_loc'
+    },
+    scope: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [fixedLocsScopes]
+      },
+      unique: 'unique_fixed_loc'
     },
     loc_string_id: {
       type: DataTypes.INTEGER,
@@ -19,6 +33,13 @@ export const up = async ({ context: queryInterface }: MigrationContext) => {
       references: { model: 'loc_strings', key: 'id' },
       onUpdate: 'CASCADE',
       onDelete: 'RESTRICT'
+    }
+  }, {
+    uniqueKeys: {
+      unique_fixed_loc: {
+        customIndex: true,
+        fields: ['name', 'namespace', 'scope']
+      }
     }
   });
 };
