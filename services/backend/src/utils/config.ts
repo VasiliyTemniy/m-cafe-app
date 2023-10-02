e97c96e53a9eadb4dcc620991027b7bbe4500843
VasiliyTemniy <flash_er@mail.ru>
2023-10-02 13:40:12 +0300

    rm DATABASE_URL resolve from backend config

--- services/backend/src/utils/config.ts
+++ services/backend/src/utils/config.ts
@@ -13,10 +13,6 @@ dotenv.config({
 
 const PORT = process.env.BACKEND_PORT as string;
 
-const DATABASE_URL = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_DATABASE_URL as string
-  : process.env.DATABASE_URL as string;
-
 const SECRET = process.env.SECRET as Secret;
 
 const TOKEN_TTL = process.env.TOKEN_TTL as string;
@@ -58,7 +54,6 @@ const sessionCookieOptions: CookieOptions = {
 
 
 export default {
-  DATABASE_URL,
   PORT,
   SECRET,
   TOKEN_TTL,
