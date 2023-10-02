54bbc8550a2bd822c3c9394aa13a5590ee492eda
VasiliyTemniy <flash_er@mail.ru>
2023-10-02 13:22:36 +0300

    add namespace, scope to FixedLoc model; add scopes

--- packages/db/src/models/FixedLoc.ts
+++ packages/db/src/models/FixedLoc.ts
@@ -1,13 +1,17 @@
 import type { InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey, NonAttribute } from 'sequelize';
-import type { PropertiesCreationOptional } from '../types/helpers.js';
+import type { PropertiesCreationOptional } from '@m-cafe-app/shared-constants';
 import { Model, DataTypes } from 'sequelize';
 import LocString from './LocString.js';
 import { sequelize } from '../db.js';
+import { Op } from 'sequelize';
+import { fixedLocFilter, fixedLocsScopes } from '@m-cafe-app/shared-constants';
 
 
 export class FixedLoc extends Model<InferAttributes<FixedLoc>, InferCreationAttributes<FixedLoc>> {
   declare id: CreationOptional<number>;
   declare name: string;
+  declare namespace: string;
+  declare scope: string;
   declare locStringId: ForeignKey<LocString['id']>;
   declare locString?: NonAttribute<LocString>;
 }
@@ -15,6 +19,17 @@ export class FixedLoc extends Model<InferAttributes<FixedLoc>, InferCreationAttr
 
 export type FixedLocData = Omit<InferAttributes<FixedLoc>, PropertiesCreationOptional>
   & { id: number; };
+
+
+const includeLocString = {
+  model: LocString,
+  as: 'locString',
+  where: {
+    mainStr: {
+      [Op.ne]: fixedLocFilter
+    }
+  }
+};
 
   
 FixedLoc.init({
@@ -26,7 +41,20 @@ FixedLoc.init({
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
+    unique: 'unique_fixed_loc'
+  },
+  namespace: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: 'unique_fixed_loc'
+  },
+  scope: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [fixedLocsScopes]
+    },
+    unique: 'unique_fixed_loc'
   },
   locStringId: {
     type: DataTypes.INTEGER,
@@ -39,7 +67,66 @@ FixedLoc.init({
   sequelize,
   underscored: true,
   timestamps: false,
-  modelName: 'fixed_loc'
+  modelName: 'fixed_loc',
+  indexes: [
+    {
+      unique: true,
+      fields: ['name', 'namespace', 'scope']
+    }
+  ],
+  defaultScope: {
+    where: {
+      scope: {
+        [Op.or]: [
+          'all',
+          'customer',
+        ]
+      }
+    },
+    include: includeLocString,
+    attributes: {
+      exclude: ['scope', 'locStringId']
+    }
+  },
+  scopes: {
+    customer: {
+      where: {
+        scope: {
+          [Op.or]: [
+            'all',
+            'customer',
+          ]
+        }
+      },
+      include: includeLocString,
+      attributes: {
+        exclude: ['scope', 'locStringId']
+      }
+    },
+    admin: {
+      where: {},
+      include: {
+        ...includeLocString,
+        // do not exclude filtered fixed locs for admin:
+        where: {}
+      }
+    },
+    manager: {
+      where: {
+        scope: {
+          [Op.ne]: 'admin'
+        }
+      },
+      include: includeLocString,
+      attributes: {
+        exclude: ['scope', 'locStringId']
+      }
+    },
+    all: {
+      where: {},
+      include: includeLocString
+    }
+  }
 });
   
 export default FixedLoc;
