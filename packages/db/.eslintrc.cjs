6a2412fc962c2561e070ad9921625d62ec6281b2
VasiliyTemniy <flash_er@mail.ru>
2023-10-02 13:32:01 +0300

    fix... quotes

--- packages/db/.eslintrc.cjs
+++ packages/db/.eslintrc.cjs
@@ -2,47 +2,47 @@ module.exports = {
   overrides: [
     {
       files: [
-        "**/*.{ts,tsx}"
+        '**/*.{ts,tsx}'
       ],
       extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
       ],
       plugins: [
-        "@typescript-eslint"
+        '@typescript-eslint'
       ],
       env: {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
+        'commonjs': true,
+        'es2021': true,
+        'node': true
       },
       rules: {
-        "@typescript-eslint/semi": [
-          "error"
+        '@typescript-eslint/semi': [
+          'error'
         ],
-        "@typescript-eslint/explicit-function-return-type": 0,
-        "@typescript-eslint/explicit-module-boundary-types": 0,
-        "@typescript-eslint/restrict-template-expressions": 0,
-        "@typescript-eslint/restrict-plus-operands": 0,
-        "@typescript-eslint/no-base-to-string": 0,
-        "@typescript-eslint/no-unsafe-member-access": 0,
-        "@typescript-eslint/no-unused-vars": [
-          "error",
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/restrict-template-expressions': 0,
+        '@typescript-eslint/restrict-plus-operands': 0,
+        '@typescript-eslint/no-base-to-string': 0,
+        '@typescript-eslint/no-unsafe-member-access': 0,
+        '@typescript-eslint/no-unused-vars': [
+          'error',
           {
-            "argsIgnorePattern": "^_"
+            'argsIgnorePattern': '^_'
           }
         ],
-        "@typescript-eslint/no-explicit-any": 1,
-        "no-useless-escape": 0,
-        "no-case-declarations": 0
+        '@typescript-eslint/no-explicit-any': 1,
+        'no-useless-escape': 0,
+        'no-case-declarations': 0
       },
-      parser: "@typescript-eslint/parser",
+      parser: '@typescript-eslint/parser',
       parserOptions: {
         ecmaVersion: 12,
-        sourceType: "module",
+        sourceType: 'module',
         project: [
-          "./tsconfig.json"
+          './tsconfig.json'
         ],
         tsconfigRootDir: __dirname
       }
