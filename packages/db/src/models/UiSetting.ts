e0da5b6038c1db479e0fb3b8b8df93c77f3e5178
VasiliyTemniy <flash_er@mail.ru>
2023-10-02 13:22:52 +0300

    add group, theme to UiSetting model; add scopes

--- packages/db/src/models/UiSetting.ts
+++ packages/db/src/models/UiSetting.ts
@@ -1,12 +1,16 @@
 import type { InferAttributes, InferCreationAttributes, CreationOptional } from 'sequelize';
-import type { PropertiesCreationOptional } from '../types/helpers.js';
+import type { PropertiesCreationOptional } from '@m-cafe-app/shared-constants';
 import { Model, DataTypes } from 'sequelize';
 import { sequelize } from '../db.js';
+import { Op } from 'sequelize';
+import { allowedThemes, componentGroups } from '@m-cafe-app/shared-constants';
 
 
 export class UiSetting extends Model<InferAttributes<UiSetting>, InferCreationAttributes<UiSetting>> {
   declare id: CreationOptional<number>;
   declare name: string;
+  declare group: string;
+  declare theme: string;
   declare value: string;
 }
 
@@ -24,7 +28,23 @@ UiSetting.init({
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
+    unique: 'unique_ui_setting'
+  },
+  group: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [componentGroups]
+    },
+    unique: 'unique_ui_setting'
+  },
+  theme: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [allowedThemes]
+    },
+    unique: 'unique_ui_setting'
   },
   value: {
     type: DataTypes.STRING,
@@ -34,7 +54,50 @@ UiSetting.init({
   sequelize,
   underscored: true,
   timestamps: false,
-  modelName: 'ui_setting'
+  modelName: 'ui_setting',
+  indexes: [
+    {
+      unique: true,
+      fields: ['name', 'group', 'theme']
+    }
+  ],
+  defaultScope: {
+    where: {
+      value: {
+        [Op.ne]: 'false'
+      }
+    }
+  },
+  scopes: {
+    light: {
+      where: {
+        theme: {
+          [Op.eq]: 'light'
+        },
+        value: {
+          [Op.ne]: 'false'
+        }
+      }
+    },
+    dark: {
+      where: {
+        theme: {
+          [Op.eq]: 'dark'
+        },
+        value: {
+          [Op.ne]: 'false'
+        }
+      }
+    },
+    nonFalsy: {
+      where: {
+        value: {
+          [Op.ne]: 'false'
+        }
+      }
+    },
+    all: {}
+  }
 });
   
 export default UiSetting;
