cb1c9052a2c1e1ac7049b9731042a1504f215ad6
VasiliyTemniy <flash_er@mail.ru>
2023-10-02 13:40:30 +0300

    upd RequestCustom

--- services/backend/src/types/RequestCustom.ts
+++ services/backend/src/types/RequestCustom.ts
@@ -1,16 +1,13 @@
 import type { Request } from 'express';
-import { hasOwnProperty, isNumber, isString } from '@m-cafe-app/utils';
+import { checkProperties, hasOwnProperty, isNumber, isString } from '@m-cafe-app/utils';
 import { User } from '@m-cafe-app/db';
 
 export interface RequestMiddle extends Request {
   userId?: number;
   token?: string;
   user?: User;
-}
-
-interface RequestCustomFields extends Request {
-  userId: unknown;
-  token: unknown;
+  rights?: string;
+  verifyOptional?: boolean;
 }
 
 export interface RequestCustom extends Request {
@@ -18,11 +15,17 @@ export interface RequestCustom extends Request {
   token: string;
 }
 
-export const hasRequestCustomFields = (req: Request): req is RequestCustomFields =>
-  hasOwnProperty(req, 'userId') && hasOwnProperty(req, 'token');
+export const isRequestCustom = (req: Request): req is RequestCustom => {
+  if (!checkProperties({obj: req, properties: [
+    'token'
+  ], required: true, validator: isString})) return false;
 
-export const isRequestCustom = (req: Request): req is RequestCustom =>
-  hasRequestCustomFields(req) && isNumber(req.userId) && isString(req.token);
+  if (!checkProperties({obj: req, properties: [
+    'userId'
+  ], required: true, validator: isNumber})) return false;
+
+  return true;
+};
 
 interface RequestWithUserFields extends RequestCustom {
   user: unknown;
@@ -37,3 +40,16 @@ export const hasRequestWithUserFields = (req: Request): req is RequestWithUserFi
 
 export const isRequestWithUser = (req: Request): req is RequestWithUser =>
   hasRequestWithUserFields(req) && isNumber(req.userId) && isString(req.token) && req.user instanceof User;
+
+
+interface RequestWithUserRights extends Request {
+  rights: string;
+}
+
+export const isRequestWithUserRights = (req: Request): req is RequestWithUserRights => {
+  if (!checkProperties({obj: req, properties: [
+    'rights'
+  ], required: true, validator: isString})) return false;
+
+  return true;
+};
