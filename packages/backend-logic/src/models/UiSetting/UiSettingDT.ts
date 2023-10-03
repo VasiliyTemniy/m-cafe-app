1a8f230c96c96e27fe6810ecba619bab1370eaaa
VasiliyTemniy <flash_er@mail.ru>
2023-10-03 19:02:23 +0300

    add UiSettingDT

---
+++ packages/backend-logic/src/models/UiSetting/UiSettingDT.ts
@@ -0,0 +1,32 @@
+import type { MapToDT, MapToDTNU } from '../../utils';
+import type { UiSetting } from './UiSetting.js';
+import { idOptional, idRequired } from '../../utils';
+import { isEntity, isManyEntity, isString } from '@m-cafe-app/utils';
+
+
+const uiSettingPropertiesGroup = {
+  properties: ['name', 'group', 'theme', 'value'],
+  required: true,
+  validator: isString,
+  isArray: false  
+};
+
+
+export type UiSettingDT = MapToDT<UiSetting>;
+
+export const isUiSettingDT = (obj: unknown): obj is UiSettingDT =>
+  isEntity(obj, [idRequired, uiSettingPropertiesGroup]);
+
+
+export const isUiSettingDTMany = (obj: unknown): obj is UiSettingDT[] =>
+  isManyEntity(obj, isUiSettingDT);
+
+
+
+export type UiSettingDTNU = MapToDTNU<UiSetting>;
+
+export const isUiSettingDTNU = (obj: unknown): obj is UiSettingDTNU =>
+  isEntity(obj, [idOptional, uiSettingPropertiesGroup]);
+
+export const isUiSettingDTNUMany = (obj: unknown): obj is UiSettingDTNU[] =>
+  isManyEntity(obj, isUiSettingDTNU);
