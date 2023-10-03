8c7bc339ad4e9d9e2a3c68de24c367d01edb4f4e
VasiliyTemniy <flash_er@mail.ru>
2023-10-03 19:05:41 +0300

    use UiSetting Controller inside Routes

--- services/backend/src/controllers/uiSetting.ts
+++ services/backend/src/controllers/uiSetting.ts
@@ -1,18 +1,17 @@
-import type { UiSettingDT } from '@m-cafe-app/utils';
 import type { RequestHandler } from 'express';
-import {
-  DatabaseError,
-  isEditUiSettingBody,
-  isNewUiSettingBody,
-  mapDataToTransit,
-  RequestBodyError,
-  isEditManyUiSettingBody,
-  UnknownError
-} from '@m-cafe-app/utils';
+import { UnknownError } from '@m-cafe-app/utils';
 import { Router } from 'express';
-import { UiSetting } from '@m-cafe-app/db';
 import middleware from '../utils/middleware.js';
 import { isRequestWithUserRights } from '../types/RequestCustom.js';
+import { UiSettingControllerExpressHttp, UiSettingRepoSequelizePG, UiSettingService } from '@m-cafe-app/backend-logic';
+
+
+const uiSettingController =
+  new UiSettingControllerExpressHttp(
+    new UiSettingService(
+      new UiSettingRepoSequelizePG()
+    )
+  );
 
 
 const uiSettingRouter = Router();
@@ -26,46 +25,19 @@ uiSettingRouter.get(
 
     if (!isRequestWithUserRights(req)) throw new UnknownError('This code should never be reached - check userRightsExtractor middleware');
 
-    const scope = req.rights === 'admin'
-      ? 'all'
-      : 'nonFalsy';
-
-    const uiSettings = await UiSetting.scope(scope).findAll();
-
-    const resBody: UiSettingDT[] = uiSettings.map(uiSetting => {
-      const resUiSetting: UiSettingDT = {
-        ...mapDataToTransit(uiSetting.dataValues)
-      };
-      return resUiSetting;
-    });
-    
-    res.status(200).json(resBody);
+    await uiSettingController.getAllByScope(req, res);
 
   }) as RequestHandler
 );
 
 uiSettingRouter.get(
   '/:id',
-  middleware.setVerifyOptional,
   middleware.verifyToken,
-  middleware.userRightsExtractor,
-  middleware.requestParamsCheck,
+  middleware.adminCheck,
+  middleware.sessionCheck,
   (async (req, res) => {
 
-    if (!isRequestWithUserRights(req)) throw new UnknownError('This code should never be reached - check userRightsExtractor middleware');
-
-    const scope = req.rights === 'admin'
-      ? 'all'
-      : 'nonFalsy';
-
-    const uiSetting = await UiSetting.scope(scope).findByPk(req.params.id);
-    if (!uiSetting) throw new DatabaseError(`No ui setting entry with this id ${req.params.id}`);
-
-    const resBody: UiSettingDT = {
-      ...mapDataToTransit(uiSetting.dataValues)
-    };
-    
-    res.status(200).json(resBody);
+    await uiSettingController.getById(req, res);
 
   }) as RequestHandler
 );
@@ -79,22 +51,7 @@ uiSettingRouter.post(
   middleware.sessionCheck,
   (async (req, res) => {
 
-    if (!isNewUiSettingBody(req.body)) throw new RequestBodyError('Invalid new ui setting request body');
-
-    const { name, value, theme, group } = req.body;
-    
-    const savedUiSetting = await UiSetting.create({
-      name,
-      value,
-      theme,
-      group
-    });
-
-    const resBody: UiSettingDT = {
-      ...mapDataToTransit(savedUiSetting.dataValues)
-    };
-    
-    res.status(201).json(resBody);
+    await uiSettingController.create(req, res);
 
   }) as RequestHandler
 );
@@ -107,33 +64,7 @@ uiSettingRouter.put(
   middleware.requestParamsCheck,
   (async (req, res) => {
 
-    if (!isEditManyUiSettingBody(req.body)) throw new RequestBodyError('Invalid edit many ui settings request body');
-
-    const { updUiSettings } = req.body;
-
-    const resUpdUiSettings = [] as UiSetting[];
-
-    for (const updUiSetting of updUiSettings) {
-
-      const resUpdUiSetting = await UiSetting.scope('all').findByPk(updUiSetting.id);
-      if (!resUpdUiSetting) throw new DatabaseError(`No ui setting entry with this id ${updUiSetting.id}`);
-
-      resUpdUiSetting.value = updUiSetting.value;
-
-      await resUpdUiSetting.save();
-
-      resUpdUiSettings.push(resUpdUiSetting);
-
-    }
-
-    const resBody: UiSettingDT[] = resUpdUiSettings.map(updUiSetting => {
-      const resUiSetting: UiSettingDT = {
-        ...mapDataToTransit(updUiSetting.dataValues)
-      };
-      return resUiSetting;
-    });
-    
-    res.status(200).json(resBody);
+    await uiSettingController.updateMany(req, res);
 
   }) as RequestHandler
 );
@@ -146,24 +77,7 @@ uiSettingRouter.put(
   middleware.requestParamsCheck,
   (async (req, res) => {
 
-    if (!isEditUiSettingBody(req.body)) throw new RequestBodyError('Invalid edit ui setting request body');
-
-    const { name, value } = req.body;
-    const _name = name;
-
-    const updUiSetting = await UiSetting.scope('all').findByPk(req.params.id);
-    if (!updUiSetting) throw new DatabaseError(`No ui setting entry with this id ${req.params.id}`);
-
-    // updUiSetting.name = name; // Ui settings names must not be mutated!
-    updUiSetting.value = value;
-
-    await updUiSetting.save();
-
-    const resBody: UiSettingDT = {
-      ...mapDataToTransit(updUiSetting.dataValues)
-    };
-    
-    res.status(200).json(resBody);
+    await uiSettingController.update(req, res);
 
   }) as RequestHandler
 );
@@ -176,11 +90,174 @@ uiSettingRouter.delete(
   middleware.requestParamsCheck,
   (async (req, res) => {
 
-    await UiSetting.destroy({ where: { id: req.params.id } });
-
-    res.status(204).end();
+    await uiSettingController.remove(req, res);
 
   }) as RequestHandler
 );
+
+// uiSettingRouter.get(
+//   '/',
+//   middleware.setVerifyOptional,
+//   middleware.verifyToken,
+//   middleware.userRightsExtractor,
+//   (async (req, res) => {
+
+//     if (!isRequestWithUserRights(req)) throw new UnknownError('This code should never be reached - check userRightsExtractor middleware');
+
+//     const scope = req.rights === 'admin'
+//       ? 'all'
+//       : 'nonFalsy';
+
+//     const uiSettings = await UiSetting.scope(scope).findAll();
+
+//     const resBody: UiSettingDT[] = uiSettings.map(uiSetting => {
+//       const resUiSetting: UiSettingDT = {
+//         ...mapDataToTransit(uiSetting.dataValues)
+//       };
+//       return resUiSetting;
+//     });
+    
+//     res.status(200).json(resBody);
+
+//   }) as RequestHandler
+// );
+
+// uiSettingRouter.get(
+//   '/:id',
+//   middleware.verifyToken,
+//   middleware.adminCheck,
+//   middleware.sessionCheck,
+//   (async (req, res) => {
+
+//     if (!isRequestWithUserRights(req)) throw new UnknownError('This code should never be reached - check userRightsExtractor middleware');
+
+//     const scope = req.rights === 'admin'
+//       ? 'all'
+//       : 'nonFalsy';
+
+//     const uiSetting = await UiSetting.scope(scope).findByPk(req.params.id);
+//     if (!uiSetting) throw new DatabaseError(`No ui setting entry with this id ${req.params.id}`);
+
+//     const resBody: UiSettingDT = {
+//       ...mapDataToTransit(uiSetting.dataValues)
+//     };
+    
+//     res.status(200).json(resBody);
+
+//   }) as RequestHandler
+// );
+
+
+// // Should not ever be used. All ui settings must be initialized after backend start and then must only have mutable values
+// uiSettingRouter.post(
+//   '/',
+//   middleware.verifyToken,
+//   middleware.adminCheck,
+//   middleware.sessionCheck,
+//   (async (req, res) => {
+
+//     if (!isNewUiSettingBody(req.body)) throw new RequestBodyError('Invalid new ui setting request body');
+
+//     const { name, value, theme, group } = req.body;
+    
+//     const savedUiSetting = await UiSetting.create({
+//       name,
+//       value,
+//       theme,
+//       group
+//     });
+
+//     const resBody: UiSettingDT = {
+//       ...mapDataToTransit(savedUiSetting.dataValues)
+//     };
+    
+//     res.status(201).json(resBody);
+
+//   }) as RequestHandler
+// );
+
+// uiSettingRouter.put(
+//   '/all',
+//   middleware.verifyToken,
+//   middleware.adminCheck,
+//   middleware.sessionCheck,
+//   middleware.requestParamsCheck,
+//   (async (req, res) => {
+
+//     if (!isEditManyUiSettingBody(req.body)) throw new RequestBodyError('Invalid edit many ui settings request body');
+
+//     const { updUiSettings } = req.body;
+
+//     const resUpdUiSettings = [] as UiSetting[];
+
+//     for (const updUiSetting of updUiSettings) {
+
+//       const resUpdUiSetting = await UiSetting.scope('all').findByPk(updUiSetting.id);
+//       if (!resUpdUiSetting) throw new DatabaseError(`No ui setting entry with this id ${updUiSetting.id}`);
+
+//       resUpdUiSetting.value = updUiSetting.value;
+
+//       await resUpdUiSetting.save();
+
+//       resUpdUiSettings.push(resUpdUiSetting);
+
+//     }
+
+//     const resBody: UiSettingDT[] = resUpdUiSettings.map(updUiSetting => {
+//       const resUiSetting: UiSettingDT = {
+//         ...mapDataToTransit(updUiSetting.dataValues)
+//       };
+//       return resUiSetting;
+//     });
+    
+//     res.status(200).json(resBody);
+
+//   }) as RequestHandler
+// );
+
+// uiSettingRouter.put(
+//   '/:id',
+//   middleware.verifyToken,
+//   middleware.adminCheck,
+//   middleware.sessionCheck,
+//   middleware.requestParamsCheck,
+//   (async (req, res) => {
+
+//     if (!isEditUiSettingBody(req.body)) throw new RequestBodyError('Invalid edit ui setting request body');
+
+//     const { name, value } = req.body;
+//     const _name = name;
+
+//     const updUiSetting = await UiSetting.scope('all').findByPk(req.params.id);
+//     if (!updUiSetting) throw new DatabaseError(`No ui setting entry with this id ${req.params.id}`);
+
+//     // updUiSetting.name = name; // Ui settings names must not be mutated!
+//     updUiSetting.value = value;
+
+//     await updUiSetting.save();
+
+//     const resBody: UiSettingDT = {
+//       ...mapDataToTransit(updUiSetting.dataValues)
+//     };
+    
+//     res.status(200).json(resBody);
+
+//   }) as RequestHandler
+// );
+
+// uiSettingRouter.delete(
+//   '/:id',
+//   middleware.verifyToken,
+//   middleware.superAdminCheck,
+//   middleware.sessionCheck,
+//   middleware.requestParamsCheck,
+//   (async (req, res) => {
+
+//     await UiSetting.destroy({ where: { id: req.params.id } });
+
+//     res.status(204).end();
+
+//   }) as RequestHandler
+// );
 
 export default uiSettingRouter;
