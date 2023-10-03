72b9440506003c6fe94fb2aef2012e4c5f36de64
VasiliyTemniy <flash_er@mail.ru>
2023-10-03 21:03:52 +0300

    minor fix

--- packages/backend-logic/src/utils/ICRUD.ts
+++ packages/backend-logic/src/utils/ICRUD.ts
@@ -4,22 +4,24 @@ import type { MapToDTNU } from './typeMappers.js';
 export interface ICRUDRepo<T> {
   getAll(): Promise<T[]>;
   getById(id: number): Promise<T>;
-  create?(dt: MapToDTNU<T>): Promise<T>;
+  create(dt: MapToDTNU<T>): Promise<T>;
+  update(dt: MapToDTNU<T>): Promise<T>;
+  remove(id: number): Promise<void>;
+  removeAll(): Promise<void>;
   createMany?(dts: MapToDTNU<T>[]): Promise<T[]>;
-  update?(dt: MapToDTNU<T>): Promise<T>;
   updateMany?(dts: MapToDTNU<T>[]): Promise<T[]>;
-  remove?(id: number): Promise<void>;
   removeMany?(ids: number[]): Promise<void>;
 }
 
 export interface ICRUDService<DT> {
   getAll(...args: any): Promise<DT[]>;
   getById(...args: any): Promise<DT>;
-  create?(...args: any): Promise<DT>;
+  create(...args: any): Promise<DT>;
+  update(...args: any): Promise<DT>;
+  remove(...args: any): Promise<void>;
+  removeAll(): Promise<void>;
   createMany?(...args: any): Promise<DT[]>;
-  update?(...args: any): Promise<DT>;
   updateMany?(...args: any): Promise<DT[]>;
-  remove?(...args: any): Promise<void>;
   removeMany?(...args: any): Promise<void>;
 }
 
@@ -27,9 +29,9 @@ export interface ICRUDController {
   getAll(...args: any): Promise<void>;
   getById(...args: any): Promise<void>;
   create?(...args: any): Promise<void>;
-  createMany?(...args: any): Promise<void>;
   update?(...args: any): Promise<void>;
-  updateMany?(...args: any): Promise<void>;
   remove?(...args: any): Promise<void>;
+  createMany?(...args: any): Promise<void>;
+  updateMany?(...args: any): Promise<void>;
   removeMany?(...args: any): Promise<void>;
 }
