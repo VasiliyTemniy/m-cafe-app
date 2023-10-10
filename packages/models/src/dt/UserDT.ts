d31cc94a8ecc99c27c58f666fff6fe8ed758254d
VasiliyTemniy <flash_er@mail.ru>
2023-10-10 22:55:15 +0300

    upd User transit data types and validators

--- packages/models/src/dt/UserDT.ts
+++ packages/models/src/dt/UserDT.ts
@@ -6,7 +6,8 @@ import {
   ApplicationError,
   isEntity,
   idRequired,
-  passwordRequired
+  passwordRequired,
+  isUnknownObject
 } from '@m-cafe-app/utils';
 
 
@@ -22,8 +23,7 @@ const userDTPropertiesGroups = [{
   isArray: false
 }];
 
-
-export type UserDT = Omit<MapToDT<User>, 'password'>;
+export type UserDT = Omit<MapToDT<User>, 'lookupHash' | 'lookupNoise'>;
 
 /**
  * Checks if the given object is of type UserDT. Throws an error if it has passwordHash.
@@ -35,16 +35,20 @@ export const isUserDT = (obj: unknown): obj is UserDT => {
 };
 
 
-const newPasswordOptional = {
-  properties: ['newPassword'],
+const userDTNUPropertiesGroups = [{
+  properties: ['phonenumber'],
+  required: true,
+  validator: isString,
+  isArray: false
+}, {
+  properties: ['username', 'name', 'email', 'birthdate'],
   required: false,
   validator: isString,
   isArray: false
-};
+}];
 
-export type UserDTN = MapToDTN<User> & {
+export type UserDTN = Omit<MapToDTN<User>, 'lookupHash' | 'lookupNoise' | 'rights'> & {
   password: string;
-  newPassword?: string;
 };
 
 /**
@@ -53,5 +57,45 @@ export type UserDTN = MapToDTN<User> & {
 export const isUserDTN = (obj: unknown): obj is UserDTN => {
   if (hasOwnProperty(obj, 'passwordHash')) throw new ApplicationError('User data transit passwordHash detected! Please, contact admins');
 
-  return isEntity(obj, [ passwordRequired, newPasswordOptional, ...userDTPropertiesGroups ]);
+  return isEntity(obj, [ passwordRequired, ...userDTNUPropertiesGroups ]);
+};
+
+
+
+const newPasswordOptional = {
+  properties: ['newPassword'],
+  required: false,
+  validator: isString,
+  isArray: false
+};
+
+export type UserDTU = Omit<MapToDT<User>, 'lookupHash' | 'lookupNoise' | 'rights'> & {
+  password: string;
+  newPassword?: string;
+};
+
+export const isUserDTU = (obj: unknown): obj is UserDTU => {
+  if (hasOwnProperty(obj, 'passwordHash')) throw new ApplicationError('User data transit passwordHash detected! Please, contact admins');
+
+  return isEntity(obj, [ passwordRequired, newPasswordOptional, ...userDTNUPropertiesGroups ]);
+};
+
+
+export type UserLoginDT = {
+  phonenumber?: string;
+  username?: string;
+  email?: string;
+  password: string;
+};
+
+export const isUserLoginDT = (obj: unknown): obj is UserLoginDT => {
+  if (!isUnknownObject(obj)) return false;
+
+  if (!hasOwnProperty(obj, 'password')) return false;
+
+  if (hasOwnProperty(obj, 'phonenumber') && isString(obj.phonenumber)) return true;
+  if (hasOwnProperty(obj, 'username') && isString(obj.username)) return true;
+  if (hasOwnProperty(obj, 'email') && isString(obj.email)) return true;
+
+  return false;
 };
