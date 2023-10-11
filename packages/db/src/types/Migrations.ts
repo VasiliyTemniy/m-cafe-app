53e164bfc1269dcba8a6f6bdde43e246ecdf0848
VasiliyTemniy <flash_er@mail.ru>
2023-10-11 21:10:49 +0300

    rename MigrationContext.ts -> Migrations.ts, add export type MigrationFn

--- packages/db/src/types/Migrations.ts
+++ packages/db/src/types/Migrations.ts
@@ -3,3 +3,5 @@ import { QueryInterface } from 'sequelize';
 export interface MigrationContext {
   context: QueryInterface;
 }
+
+export type MigrationFn = ({ context }: MigrationContext) => Promise<void>;
