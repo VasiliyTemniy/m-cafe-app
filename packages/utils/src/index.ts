0ae4d41439d782fcb5d81b4b5ae0e21e79e47197
VasiliyTemniy <flash_er@mail.ru>
2023-10-13 21:10:43 +0300

    upd index

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,6 +3,5 @@ export * from './types/requestBodies/index.js';
 export * from './types/responseBodies/index.js';
 export * from './types/typeValidators.js';
 export * from './types/helpers.js';
-export * from './types/validationHelpers.js';
 export * from './models/index.js';
 export * from './logger.js';
