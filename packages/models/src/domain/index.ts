4536b8587f0c7a445764d844fd1eee5432fc6850
VasiliyTemniy <flash_er@mail.ru>
2023-10-13 21:11:50 +0300

    upd index

--- packages/models/src/domain/index.ts
+++ packages/models/src/domain/index.ts
@@ -2,3 +2,5 @@ export * from './UiSetting.js';
 export * from './User.js';
 export * from './Auth.js';
 export * from './Session.js';
+export * from './LocString.js';
+export * from './FixedLoc.js';
--- packages/models/src/dt/index.ts
+++ packages/models/src/dt/index.ts
@@ -1,3 +1,5 @@
 export * from './UiSettingDT.js';
 export * from './UserDT.js';
 export * from './AuthDT.js';
+export * from './LocStringDT.js';
+export * from './FixedLocDT.js';
