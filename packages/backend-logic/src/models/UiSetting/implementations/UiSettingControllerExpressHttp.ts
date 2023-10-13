8107543bd2aeb9c1f66c3ff3d06ad23d22cc86f9
VasiliyTemniy <flash_er@mail.ru>
2023-10-13 15:29:42 +0300

    update getByScope to use inmem in case user is not admin; add explicit response types

--- packages/backend-logic/src/models/UiSetting/implementations/UiSettingControllerExpressHttp.ts
+++ packages/backend-logic/src/models/UiSetting/implementations/UiSettingControllerExpressHttp.ts
@@ -1,39 +1,49 @@
 import type { IUiSettingControllerHttp, IUiSettingService } from '../interfaces';
 import type { Request, Response } from 'express';
 import type { RequestWithUserRights } from '../../../utils';
+import type { UiSettingDT, UiSettingInmemDT } from '@m-cafe-app/models';
 import { isUiSettingDTMany, isUiSettingDTN } from '@m-cafe-app/models';
-import { ApplicationError, RequestBodyError } from '@m-cafe-app/utils';
+import { ApplicationError, RequestBodyError, isString } from '@m-cafe-app/utils';
 
 
 export class UiSettingControllerExpressHttp implements IUiSettingControllerHttp {
   constructor( readonly service: IUiSettingService ) {}
 
-  async getAll(req: Request, res: Response) {
-    const uiSettings = await this.service.getAll();
+  async getAll(req: Request, res: Response): Promise<void> {
+    const uiSettings: UiSettingDT[] = await this.service.getAll();
     res.status(200).json(uiSettings);
   }
 
-  async getById(req: Request, res: Response) {
-    const uiSetting = await this.service.getById(Number(req.params.id));
+  async getById(req: Request, res: Response): Promise<void> {
+    const uiSetting: UiSettingDT = await this.service.getById(Number(req.params.id));
     res.status(200).json(uiSetting);
   }
 
-  async getByScope(req: RequestWithUserRights, res: Response) {
-    const scope = req.rights === 'admin'
-      ? 'all'
-      : 'nonFalsy';
+  async getByScope(req: RequestWithUserRights, res: Response): Promise<void> {
+    if (req.rights === 'admin') {
+      let scope = 'all';
 
-    const uiSettings = await this.service.getByScope(scope);
-    res.status(200).json(uiSettings);
+      if (isString(req.query.scope)) scope = req.query.scope;
+
+      const uiSettings: UiSettingDT[] = await this.service.getByScope(scope);
+      res.status(200).json(uiSettings);
+    } else {
+      let theme = '';
+
+      if (isString(req.query.theme)) theme = req.query.theme;
+
+      const uiSettings: UiSettingInmemDT[] = await this.service.getFromInmem(theme);
+      res.status(200).json(uiSettings);
+    }
   }
 
-  async create(req: Request, res: Response) {
+  async create(req: Request, res: Response): Promise<void> {
     if (!isUiSettingDTN(req.body))
       throw new RequestBodyError('Invalid new ui setting request body');
 
     const { name, value, theme, group } = req.body;
 
-    const savedUiSetting = await this.service.create({
+    const savedUiSetting: UiSettingDT = await this.service.create({
       name,
       value,
       theme,
@@ -43,13 +53,13 @@ export class UiSettingControllerExpressHttp implements IUiSettingControllerHttp 
     res.status(201).json(savedUiSetting);
   }
 
-  async update(req: Request, res: Response) {
+  async update(req: Request, res: Response): Promise<void> {
     if (!isUiSettingDTN(req.body))
       throw new RequestBodyError('Invalid edit ui setting request body');
 
     const { name, value, theme, group } = req.body;
 
-    const updatedUiSetting = await this.service.update({
+    const updatedUiSetting: UiSettingDT = await this.service.update({
       id: Number(req.params.id),
       name,
       value,
@@ -60,7 +70,7 @@ export class UiSettingControllerExpressHttp implements IUiSettingControllerHttp 
     res.status(200).json(updatedUiSetting);
   }
 
-  async updateMany(req: Request, res: Response) {
+  async updateMany(req: Request, res: Response): Promise<void> {
     if (!isUiSettingDTMany(req.body))
       throw new RequestBodyError('Invalid edit many ui settings request body');
 
@@ -69,14 +79,14 @@ export class UiSettingControllerExpressHttp implements IUiSettingControllerHttp 
     if (!this.service.updateMany)
       throw new ApplicationError(`Update many method not implemented for service ${this.service.constructor.name}`);
 
-    const updatedUiSettings = await this.service.updateMany(updUiSettings);
+    const updatedUiSettings: UiSettingDT[] = await this.service.updateMany(updUiSettings);
     if (!updatedUiSettings) throw new ApplicationError('Could not update ui settings, check service implementation');
 
     res.status(200).json(updatedUiSettings);
   }
 
   async reset(req: Request, res: Response): Promise<void> {
-    const resettedUiSettings = await this.service.reset();
+    const resettedUiSettings: UiSettingDT[] = await this.service.reset();
 
     res.status(200).json(resettedUiSettings);
   }
