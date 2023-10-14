a5c9868520ac3129766d78aa9d247b539d3bbc66
VasiliyTemniy <flash_er@mail.ru>
2023-10-14 15:01:04 +0300

    upd InmemRepo interface: separate base and generic

--- packages/backend-logic/src/utils/IInmemRepo.ts
+++ packages/backend-logic/src/utils/IInmemRepo.ts
@@ -1,14 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 /**
- * Gets T to store, returns simple version TS, usually with cut id
+ * Shared inmem repo methods
  */
-export interface IInmemRepo<T, TS> {
-  getMany(...filterArgs: any): Promise<TS[]>;
-  storeMany(ds: T[]): Promise<void>;
-  remove(key: string): Promise<void>;
+export interface IInmemRepoBase {
   removeAll(): Promise<void>;
   connect(): Promise<void>;
   ping(): Promise<void>;
   close(): Promise<void>;
+}
+
+/**
+ * Gets T to store, returns simple version TS, usually with cut id
+ */
+export interface IInmemRepo<T, TS> extends IInmemRepoBase {
+  getMany(...filterArgs: any): Promise<TS[]>;
+  storeMany(ds: T[], ...args: any): Promise<void>;
+  remove(identifier: string | number, ...args: any): Promise<void>;
 }
