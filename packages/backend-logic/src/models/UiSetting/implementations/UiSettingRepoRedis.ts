2c5733894eb6581e1d619bb52584441a3aff8cf6
VasiliyTemniy <flash_er@mail.ru>
2023-10-14 15:03:15 +0300

    apply generic RedisRepoBase

--- packages/backend-logic/src/models/UiSetting/implementations/UiSettingRepoRedis.ts
+++ packages/backend-logic/src/models/UiSetting/implementations/UiSettingRepoRedis.ts
@@ -1,15 +1,14 @@
 import type { UiSetting, UiSettingS } from '@m-cafe-app/models';
 import type { IUiSettingSRepo } from '../interfaces';
-import { logger } from '@m-cafe-app/utils';
-import { redisUiSettingsClient } from '../../../config';
+import { RedisRepoBase } from '../../../utils';
 
-export class UiSettingRepoRedis implements IUiSettingSRepo {
+export class UiSettingRepoRedis extends RedisRepoBase implements IUiSettingSRepo {
 
   async getMany(theme?: string): Promise<UiSettingS[]> {
     const uiSettingsInmem: UiSettingS[] = [];
 
-    for await (const key of redisUiSettingsClient.scanIterator()) {
-      const response = await redisUiSettingsClient.hGetAll(key);
+    for await (const key of this.redisClient.scanIterator()) {
+      const response = await this.redisClient.hGetAll(key);
 
       if (theme && response.theme !== theme)
         continue;
@@ -26,7 +25,7 @@ export class UiSettingRepoRedis implements IUiSettingSRepo {
   }
 
   async storeMany(uiSettings: UiSetting[]): Promise<void> {
-    const multi = redisUiSettingsClient.multi();
+    const multi = this.redisClient.multi();
 
     for (const uiSetting of uiSettings) {
       multi.hSet(`${uiSetting.name}`, { value: uiSetting.value, group: uiSetting.group, theme: uiSetting.theme });
@@ -36,46 +35,6 @@ export class UiSettingRepoRedis implements IUiSettingSRepo {
   }
 
   async remove(name: string): Promise<void> {
-    await redisUiSettingsClient.del(name);
+    await this.redisClient.del(name);
   }
-
-  async removeAll(): Promise<void> {
-    await redisUiSettingsClient.flushDb();
-  }
-
-  async connect(): Promise<void> {
-    try {
-      await redisUiSettingsClient.connect();
-      logger.info('connected to redis');
-    } catch (err) {
-      logger.error(err as string);
-      logger.info('failed to connect to redis');
-      if (process.env.NODE_ENV === 'production') {
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-        await this.connect();
-      }
-      return process.exit(1);
-    }
-  }
-
-  async ping(): Promise<void> {
-    try {
-      await redisUiSettingsClient.ping();
-    } catch (err) {
-      logger.error(err as string);
-      logger.info('failed to ping redis');
-      if (process.env.NODE_ENV === 'production') {
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-        await this.close();
-        await this.connect();
-        await this.ping();
-      }
-      return process.exit(1);
-    }
-  }
-
-  async close(): Promise<void> {
-    await redisUiSettingsClient.quit();
-  }
-
 }
