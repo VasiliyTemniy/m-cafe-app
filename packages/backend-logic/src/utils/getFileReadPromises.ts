1e74d2026e1079e3a06b9d0635ecfe3dcf8de2d2
VasiliyTemniy <flash_er@mail.ru>
2023-10-15 13:19:03 +0300

    seems to work now without all the complicated stuff

--- packages/backend-logic/src/utils/getFileReadPromises.ts
+++ packages/backend-logic/src/utils/getFileReadPromises.ts
@@ -1,32 +1,20 @@
 import { glob } from 'glob';
 import { readFile } from 'fs/promises';
+import path from 'path';
 
 /**
- * @param path dirname like locales or src/controllers
+ * @param dirPath dirname like locales or src/controllers
  * @param ext file extension
  * @returns Promise of file contents strings
  */
-export const getFileReadPromises = async (path: string, ext: string) => {
+export const getFileReadPromises = async (dirPath: string, ext: string) => {
 
-  // Rewrite this later
-
-  const prodGlobPath = `${path}/`;
-  const devRelativePath = `${path}/`;
-  const devRelativePathWin32 = `${path}\\`;
-  const ReplacePath = `./${path}/`;
-
-  // Find all locales files
-  const res = process.env.NODE_ENV === 'production'
-    ? await glob(`${prodGlobPath}*.${ext}`)
-    : await glob(`${devRelativePath}*.${ext}`);
+  const res = await glob(`${dirPath}/*.${ext}`);
 
   // Read all locales files
   const fileReadPromises = res.map(async (file) => {
-    // Replace path is different for windows and in production src -> build
-    const replacePath = process.platform === 'win32' ? devRelativePathWin32 :
-      process.env.NODE_ENV === 'production' ? prodGlobPath : devRelativePath;
 
-    const fileReadPromise = await readFile(file.replace(replacePath, ReplacePath), 'utf8');
+    const fileReadPromise = await readFile(path.resolve(file), 'utf8');
 
     return fileReadPromise;
   });
