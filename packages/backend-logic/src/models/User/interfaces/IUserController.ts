b866075c3aa194bd6c5d14c237ff191a5e457d3e
VasiliyTemniy <flash_er@mail.ru>
2023-10-23 20:00:39 +0300

    add Address-specific methods

--- packages/backend-logic/src/models/User/interfaces/IUserController.ts
+++ packages/backend-logic/src/models/User/interfaces/IUserController.ts
@@ -3,17 +3,25 @@ import type { ICRUDController, ICRUDControllerHttp } from '../../../utils';
 import type { Request, Response } from 'express';
 
 export interface IUserController extends ICRUDController {
-  getByScope(...args: any): Promise<void>
-  getSome(...args: any): Promise<void>
-  administrate(...args: any): Promise<void>
-  login(...args: any): Promise<void>
-  logout(...args: any): Promise<void>
+  getByScope(...args: any): Promise<void>;
+  getSome(...args: any): Promise<void>;
+  administrate(...args: any): Promise<void>;
+  login(...args: any): Promise<void>;
+  logout(...args: any): Promise<void>;
+  createAddress(...args: any): Promise<void>;
+  updateAddress(...args: any): Promise<void>;
+  removeAddress(...args: any): Promise<void>;
+  getWithAddress(...args: any): Promise<void>;
 }
 
 export interface IUserControllerHttp extends ICRUDControllerHttp {
-  getByScope(req: Request, res: Response): Promise<void>
-  getSome(req: Request, res: Response): Promise<void>
-  administrate(req: Request, res: Response): Promise<void>
-  login(req: Request, res: Response): Promise<void>
-  logout(req: Request, res: Response): Promise<void>
+  getByScope(req: Request, res: Response): Promise<void>;
+  getSome(req: Request, res: Response): Promise<void>;
+  administrate(req: Request, res: Response): Promise<void>;
+  login(req: Request, res: Response): Promise<void>;
+  logout(req: Request, res: Response): Promise<void>;
+  createAddress(req: Request, res: Response): Promise<void>;
+  updateAddress(req: Request, res: Response): Promise<void>;
+  removeAddress(req: Request, res: Response): Promise<void>;
+  getWithAddress(req: Request, res: Response): Promise<void>;
 } 
