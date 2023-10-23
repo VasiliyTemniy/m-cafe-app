c37f5b7df823d0587a9c916366be27b07980728a
VasiliyTemniy <flash_er@mail.ru>
2023-10-23 20:00:25 +0300

    add Address-specific methods

--- packages/backend-logic/src/models/User/interfaces/IUserService.ts
+++ packages/backend-logic/src/models/User/interfaces/IUserService.ts
@@ -1,23 +1,34 @@
-import type { AuthResponse, UserDT, UserDTN, UserDTU, UserUniqueProperties } from '@m-cafe-app/models';
+import type {
+  AuthResponse,
+  UserDT,
+  UserDTN,
+  UserDTU,
+  UserUniqueProperties,
+  AdministrateUserBody,
+  AddressDTN,
+  AddressDT
+} from '@m-cafe-app/models';
 import type { ICRUDService } from '../../../utils';
-import type { AdministrateUserBody } from '@m-cafe-app/utils';
 
 
 export interface IUserService extends Omit<ICRUDService<UserDT, UserDTN>, 'create' | 'update'> {
-  getByScope(scope: string): Promise<UserDT[]>
-  getSome(limit: number, offset: number): Promise<UserDT[]>
-  create(userDTN: UserDTN, userAgent: string): Promise<{ user: UserDT, auth: AuthResponse}>
-  update(userDTU: UserDTU, userAgent: string): Promise<{ user: UserDT, auth: AuthResponse}>
+  getByScope(scope: string): Promise<UserDT[]>;
+  getSome(limit: number, offset: number): Promise<UserDT[]>;
+  create(userDTN: UserDTN, userAgent: string): Promise<{ user: UserDT, auth: AuthResponse}>;
+  update(userDTU: UserDTU, userAgent: string): Promise<{ user: UserDT, auth: AuthResponse}>;
   authenticate(
     password: string,
     credential: UserUniqueProperties,
     userAgent: string
-  ): Promise<{ user: UserDT, auth: AuthResponse }>
-  logout(id: number, userAgent: string): Promise<void>
-  administrate(id: number, body: AdministrateUserBody): Promise<UserDT>
-  remove(id: number): Promise<UserDT>
-  delete(id: number): Promise<void>
-  initSuperAdmin(): Promise<void>
-  // resolveAuthLookupHashConflict(user: User, password: string, tries?: number): Promise<{ user: User, auth: AuthResponse }> : private! Thank you, TS!
-  cleanSessionRepo(): Promise<void>
+  ): Promise<{ user: UserDT, auth: AuthResponse }>;
+  logout(id: number, userAgent: string): Promise<void>;
+  administrate(id: number, body: AdministrateUserBody): Promise<UserDT>;
+  remove(id: number): Promise<UserDT>;
+  delete(id: number): Promise<void>;
+  initSuperAdmin(): Promise<void>;
+  cleanSessionRepo(): Promise<void>;
+  createAddress(userId: number, address: AddressDTN): Promise<{ address: AddressDT, created: boolean }>;
+  updateAddress(userId: number, address: AddressDT): Promise<{ address: AddressDT, updated: boolean }>;
+  removeAddress(userId: number, addressId: number): Promise<void>;
+  getWithAddress(id: number): Promise<UserDT>;
 }
