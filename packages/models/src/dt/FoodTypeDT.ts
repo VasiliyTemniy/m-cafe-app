1691f4b22b2555bd039312e8131717e0569982d2
VasiliyTemniy <flash_er@mail.ru>
2023-10-24 20:04:05 +0300

    fix FoodTypeDT mapper

--- packages/models/src/dt/FoodTypeDT.ts
+++ packages/models/src/dt/FoodTypeDT.ts
@@ -1,22 +1,29 @@
-import type { MapToDT, MapToDTN, PropertyGroup } from '@m-cafe-app/utils';
+import type { MapToDT, MapToDTN } from '@m-cafe-app/utils';
 import type { FoodType } from '../domain';
+import type { LocStringDT, LocStringDTN } from './LocStringDT.js';
 import { isEntity } from '@m-cafe-app/utils';
-import { isLocStringDT } from './LocStringDT.js';
-import { idRequired } from './validationHelpers.js';
+import {
+  descriptionLocNewProperty,
+  descriptionLocProperty,
+  idRequired,
+  nameLocNewProperty,
+  nameLocProperty
+} from './validationHelpers.js';
 
 
-const foodTypePropertiesGroup: PropertyGroup = {
-  properties: ['nameLoc', 'descriptionLoc'],
-  validator: isLocStringDT,
+export type FoodTypeDT = Omit<MapToDT<FoodType>, 'nameLoc' | 'descriptionLoc'> & {
+  nameLoc: LocStringDT;
+  descriptionLoc: LocStringDT;
 };
 
-export type FoodTypeDT = MapToDT<FoodType>;
-
 export const isFoodTypeDT = (obj: unknown): obj is FoodTypeDT =>
-  isEntity(obj, [ idRequired, foodTypePropertiesGroup ]);
+  isEntity(obj, [ idRequired, nameLocProperty, descriptionLocProperty ]);
 
 
-export type FoodTypeDTN = MapToDTN<FoodType>;
+export type FoodTypeDTN = Omit<MapToDTN<FoodType>, 'nameLoc' | 'descriptionLoc'> & {
+  nameLoc: LocStringDTN;
+  descriptionLoc: LocStringDTN;
+};
 
 export const isFoodTypeDTN = (obj: unknown): obj is FoodTypeDTN =>
-  isEntity(obj, [ foodTypePropertiesGroup ]);
+  isEntity(obj, [ nameLocNewProperty, descriptionLocNewProperty ]);
