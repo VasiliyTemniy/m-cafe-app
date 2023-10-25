8747e5f08f8260d4a185760f699eb6b9e4d45bb8
VasiliyTemniy <flash_er@mail.ru>
2023-10-25 20:28:40 +0300

    use LocStringMapper

--- packages/backend-logic/src/models/FoodType/infrastructure/FoodTypeMapper.ts
+++ packages/backend-logic/src/models/FoodType/infrastructure/FoodTypeMapper.ts
@@ -3,6 +3,7 @@ import type { FoodTypeDT } from '@m-cafe-app/models';
 import { FoodType } from '@m-cafe-app/models';
 import { FoodType as FoodTypePG } from '@m-cafe-app/db';
 import { ApplicationError, toOptionalISOString } from '@m-cafe-app/utils';
+import { LocStringMapper } from '../../LocString';
 
 
 export class FoodTypeMapper implements EntityDBMapper<FoodType, FoodTypePG>, EntityDTMapper<FoodType, FoodTypeDT> {
@@ -13,22 +14,8 @@ export class FoodTypeMapper implements EntityDBMapper<FoodType, FoodTypePG>, Ent
 
     const domainFoodType = new FoodType(
       dbFoodType.id,
-      {
-        id: dbFoodType.nameLoc.id,
-        mainStr: dbFoodType.nameLoc.mainStr,
-        secStr: dbFoodType.nameLoc.secStr,
-        altStr: dbFoodType.nameLoc.altStr,
-        createdAt: dbFoodType.nameLoc.createdAt,
-        updatedAt: dbFoodType.nameLoc.updatedAt
-      },
-      {
-        id: dbFoodType.descriptionLoc.id,
-        mainStr: dbFoodType.descriptionLoc.mainStr,
-        secStr: dbFoodType.descriptionLoc.secStr,
-        altStr: dbFoodType.descriptionLoc.altStr,
-        createdAt: dbFoodType.descriptionLoc.createdAt,
-        updatedAt: dbFoodType.descriptionLoc.updatedAt
-      },
+      LocStringMapper.dbToDomain(dbFoodType.nameLoc),
+      LocStringMapper.dbToDomain(dbFoodType.descriptionLoc),
       dbFoodType.createdAt,
       dbFoodType.updatedAt
     );
@@ -42,18 +29,8 @@ export class FoodTypeMapper implements EntityDBMapper<FoodType, FoodTypePG>, Ent
   public static dtToDomain(foodTypeDT: FoodTypeDT): FoodType {
     const domainFoodType = new FoodType(
       foodTypeDT.id,
-      {
-        id: foodTypeDT.nameLoc.id,
-        mainStr: foodTypeDT.nameLoc.mainStr,
-        secStr: foodTypeDT.nameLoc.secStr,
-        altStr: foodTypeDT.nameLoc.altStr,
-      },
-      {
-        id: foodTypeDT.descriptionLoc.id,
-        mainStr: foodTypeDT.descriptionLoc.mainStr,
-        secStr: foodTypeDT.descriptionLoc.secStr,
-        altStr: foodTypeDT.descriptionLoc.altStr,
-      }
+      LocStringMapper.dtToDomain(foodTypeDT.nameLoc),
+      LocStringMapper.dtToDomain(foodTypeDT.descriptionLoc)
     );
     return domainFoodType;
   }
@@ -65,22 +42,8 @@ export class FoodTypeMapper implements EntityDBMapper<FoodType, FoodTypePG>, Ent
   public static domainToDT(domainFoodType: FoodType): FoodTypeDT {
     const foodTypeDT: FoodTypeDT = {
       id: domainFoodType.id,
-      nameLoc: {
-        id: domainFoodType.nameLoc.id,
-        mainStr: domainFoodType.nameLoc.mainStr,
-        secStr: domainFoodType.nameLoc.secStr,
-        altStr: domainFoodType.nameLoc.altStr,
-        createdAt: toOptionalISOString(domainFoodType.nameLoc.createdAt),
-        updatedAt: toOptionalISOString(domainFoodType.nameLoc.updatedAt)
-      },
-      descriptionLoc: {
-        id: domainFoodType.descriptionLoc.id,
-        mainStr: domainFoodType.descriptionLoc.mainStr,
-        secStr: domainFoodType.descriptionLoc.secStr,
-        altStr: domainFoodType.descriptionLoc.altStr,
-        createdAt: toOptionalISOString(domainFoodType.descriptionLoc.createdAt),
-        updatedAt: toOptionalISOString(domainFoodType.descriptionLoc.updatedAt)
-      },
+      nameLoc: LocStringMapper.domainToDT(domainFoodType.nameLoc),
+      descriptionLoc: LocStringMapper.domainToDT(domainFoodType.descriptionLoc),
       createdAt: toOptionalISOString(domainFoodType.createdAt),
       updatedAt: toOptionalISOString(domainFoodType.updatedAt)
     };
