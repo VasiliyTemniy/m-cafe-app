2b4f435de7d3eda29370c9c6a3a8d4fe24ca3416
VasiliyTemniy <flash_er@mail.ru>
2023-10-26 21:11:20 +0300

    upd FoodMapper - use FoodPictureMapper

--- packages/backend-logic/src/models/Food/infrastructure/FoodMapper.ts
+++ packages/backend-logic/src/models/Food/infrastructure/FoodMapper.ts
@@ -1,6 +1,3 @@
-// CAUTION! Not finished! Add PictureMapper here!!!
-
-
 import type { EntityDBMapper, EntityDTMapper } from '../../../utils';
 import type { FoodDT } from '@m-cafe-app/models';
 import { Food } from '@m-cafe-app/models';
@@ -9,6 +6,7 @@ import { ApplicationError, toOptionalISOString } from '@m-cafe-app/utils';
 import { FoodTypeMapper } from '../../FoodType';
 import { FoodComponentMapper } from '../../FoodComponent';
 import { LocStringMapper } from '../../LocString';
+import { FoodPictureMapper } from '../../FoodPicture';
 
 
 export class FoodMapper implements EntityDBMapper<Food, FoodPG>, EntityDTMapper<Food, FoodDT> {
@@ -24,14 +22,22 @@ export class FoodMapper implements EntityDBMapper<Food, FoodPG>, EntityDTMapper<
       ? dbFood.foodComponents.map(component => FoodComponentMapper.dbToDomain(component))
       : undefined;
 
+    const gallery = dbFood.gallery
+      ? dbFood.gallery.map(foodPicture => FoodPictureMapper.dbToDomain(foodPicture))
+      : undefined;
+
+    const mainPicture = gallery
+      ? gallery[0]
+      : undefined;
+
     const domainFood = new Food(
       dbFood.id,
       LocStringMapper.dbToDomain(dbFood.nameLoc),
       LocStringMapper.dbToDomain(dbFood.descriptionLoc),
       FoodTypeMapper.dbToDomain(dbFood.foodType),
       foodComponents,
-      undefined,  // ADD PictureMapper here!!! (mainPicture prop)
-      undefined,  // ADD PictureMapper here!!! (gallery prop -> many Picture)
+      mainPicture,
+      gallery,
       dbFood.createdAt,
       dbFood.updatedAt
     );
@@ -47,14 +53,22 @@ export class FoodMapper implements EntityDBMapper<Food, FoodPG>, EntityDTMapper<
       ? foodDT.foodComponents.map(component => FoodComponentMapper.dtToDomain(component))
       : undefined;
 
+    const gallery = foodDT.gallery
+      ? foodDT.gallery.map(foodPicture => FoodPictureMapper.dtToDomain(foodPicture))
+      : undefined;
+
+    const mainPicture = gallery
+      ? gallery[0]
+      : undefined;
+
     const domainFood = new Food(
       foodDT.id,
       LocStringMapper.dtToDomain(foodDT.nameLoc),
       LocStringMapper.dtToDomain(foodDT.descriptionLoc),
       FoodTypeMapper.dtToDomain(foodDT.foodType),
       foodComponents,
-      undefined,  // ADD PictureMapper here!!! (mainPicture prop)
-      undefined,  // ADD PictureMapper here!!! (gallery prop -> many Picture)
+      mainPicture,
+      gallery
       // timestamps are not accepted from frontend
       // foodDT.createdAt,
       // foodDT.updatedAt
@@ -71,14 +85,22 @@ export class FoodMapper implements EntityDBMapper<Food, FoodPG>, EntityDTMapper<
       ? domainFood.foodComponents.map(component => FoodComponentMapper.domainToDT(component))
       : undefined;
 
+    const gallery = domainFood.gallery
+      ? domainFood.gallery.map(foodPicture => FoodPictureMapper.domainToDT(foodPicture))
+      : undefined;
+
+    const mainPicture = gallery
+      ? gallery[0]
+      : undefined;
+
     const foodDT: FoodDT = {
       id: domainFood.id,
       nameLoc: LocStringMapper.domainToDT(domainFood.nameLoc),
       descriptionLoc: LocStringMapper.domainToDT(domainFood.descriptionLoc),
       foodType: FoodTypeMapper.domainToDT(domainFood.foodType),
       foodComponents,
-      mainPicture: undefined, // ADD PictureMapper here!!!
-      gallery: undefined,     // ADD PictureMapper here!!!
+      mainPicture,
+      gallery,
       createdAt: toOptionalISOString(domainFood.createdAt),
       updatedAt: toOptionalISOString(domainFood.updatedAt)
     };
