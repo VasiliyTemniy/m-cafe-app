2121791ea8ff577c287ad342a395ff86225aa8f6
VasiliyTemniy <flash_er@mail.ru>
2023-10-26 21:10:24 +0300

    fix typo

--- packages/models/src/domain/Picture.ts
+++ packages/models/src/domain/Picture.ts
@@ -3,7 +3,7 @@ import type { LocString } from './LocString.js';
 export class Picture {
   constructor (
     readonly id: number,
-    readonly url: string,
+    readonly src: string,
     readonly altTextLoc: LocString,
     readonly createdAt?: Date,
     readonly updatedAt?: Date
