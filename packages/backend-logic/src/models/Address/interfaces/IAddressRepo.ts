6bf638882dcd09af48e5d9ccbcef9e0ac00d457a
VasiliyTemniy <flash_er@mail.ru>
2023-11-08 13:56:45 +0300

    change update args to domain address, add removeIfUnused as method

--- packages/backend-logic/src/models/Address/interfaces/IAddressRepo.ts
+++ packages/backend-logic/src/models/Address/interfaces/IAddressRepo.ts
@@ -1,11 +1,19 @@
-import type { Address, AddressDT, AddressDTN } from '@m-cafe-app/models';
+import type { Address, AddressDTN } from '@m-cafe-app/models';
 import type { ICRUDRepo } from '../../../utils';
+import type { Transaction } from 'sequelize';
 
 export interface IAddressRepo extends Omit<ICRUDRepo<Address, AddressDTN>, 'create' | 'update' | 'getAll' | 'remove'> {
   getById(id: number): Promise<Address>;
   getByUserId(userId: number): Promise<Address[]>;
-  create(addressDTN: AddressDTN): Promise<{ address: Address, created: boolean }>;
-  update(addressDT: AddressDT): Promise<{ address: Address, updated: boolean }>;
+  /**
+   * Optionally accepts a transaction
+   */
+  create(addressDTN: AddressDTN, t?: Transaction): Promise<{ address: Address, created: boolean }>;
+  /**
+   * Optionally accepts a transaction
+   */
+  update(addressToUpdate: Address, t?: Transaction): Promise<{ address: Address, updated: boolean }>;
+  removeIfUnused(addressId: number): Promise<void>;
   createUserAddress(userId: number, addressId: number): Promise<{ created: boolean }>;
   updateUserAddress(userId: number, addressId: number, oldAddressId: number): Promise<{ updated: boolean }>;
   removeUserAddress(userId: number, addressId: number): Promise<void>;
