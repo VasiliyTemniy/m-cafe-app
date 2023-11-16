0be1a05dfc47ab58bf6fbad62125e016ce20418f
VasiliyTemniy <flash_er@mail.ru>
2023-11-16 18:20:32 +0300

    create, update are now accepting optional transaction with ICRUDRepo by default

--- packages/backend-logic/src/models/LocString/interfaces/ILocStringRepo.ts
+++ packages/backend-logic/src/models/LocString/interfaces/ILocStringRepo.ts
@@ -1,19 +1,10 @@
 import type { LocString, LocStringDTN } from '@m-cafe-app/models';
 import type { ICRUDRepo } from '../../../utils';
-import type { Transaction } from 'sequelize';
 
 
-export interface ILocStringRepo extends Omit<ICRUDRepo<LocString, LocStringDTN>, 'create' | 'update'> {
-  /**
-   * Optionally accepts a transaction
-   */
-  create(locString: LocStringDTN, t?: Transaction): Promise<LocString>;
-  /**
-   * Optionally accepts a transaction
-   */
-  update(locString: LocString, t?: Transaction): Promise<LocString>;
+export interface ILocStringRepo extends ICRUDRepo<LocString, LocStringDTN> {
   /**
    * Does not throw error if no loc string found with this id
    */
-  removeWithCount(id: number): Promise<number>;
+  removeWithCount(ids: number[]): Promise<number>;
 }
