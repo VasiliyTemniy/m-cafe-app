4e380d68d39b52b6dd8caa8c812b403bbafcd663
VasiliyTemniy <flash_er@mail.ru>
2023-11-16 18:07:38 +0300

    omit 'update' and 'update many' - loc string repo usage moved to service level

--- packages/backend-logic/src/models/FixedLoc/interfaces/IFixedLocRepo.ts
+++ packages/backend-logic/src/models/FixedLoc/interfaces/IFixedLocRepo.ts
@@ -1,9 +1,10 @@
-import type { FixedLoc, FixedLocDTN, FixedLocS, FixedLocUniquePropertiesGroup } from '@m-cafe-app/models';
-import type { ICRUDRepo, IInmemRepo } from '../../../utils';
+import type { FixedLoc, FixedLocDTN, FixedLocS, FixedLocUniquePropertiesGroup, LocString } from '@m-cafe-app/models';
+import type { GenericTransaction, ICRUDRepo, IInmemRepo } from '../../../utils';
 
-export interface IFixedLocRepo extends ICRUDRepo<FixedLoc, FixedLocDTN> {
-  getByScope(scope: string): Promise<FixedLoc[]>
-  getByUniqueProperties(properties: FixedLocUniquePropertiesGroup): Promise<FixedLoc | undefined>
+export interface IFixedLocRepo extends Omit<ICRUDRepo<FixedLoc, FixedLocDTN>, 'create' | 'update' | 'updateMany'> {
+  getByScope(scope: string): Promise<FixedLoc[]>;
+  getByUniqueProperties(properties: FixedLocUniquePropertiesGroup): Promise<FixedLoc | undefined>;
+  create(fixedLoc: FixedLocDTN, locString: LocString, t?: GenericTransaction): Promise<FixedLoc>;
 }
 
 export interface IFixedLocSRepo extends IInmemRepo<FixedLoc, FixedLocS> {
