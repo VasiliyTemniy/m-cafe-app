c6659c7cbcef2b2c16f511f07dd73577ceb29bb4
VasiliyTemniy <flash_er@mail.ru>
2023-11-16 18:21:34 +0300

    move transaction handling to service level; no need for ternaries for transaction

--- packages/backend-logic/src/models/LocString/implementations/LocStringRepoSequelizePG.ts
+++ packages/backend-logic/src/models/LocString/implementations/LocStringRepoSequelizePG.ts
@@ -1,22 +1,12 @@
 import type { ILocStringRepo } from '../interfaces';
 import type { LocStringDTN } from '@m-cafe-app/models';
-import type { IDatabaseConnectionHandler } from '@m-cafe-app/db';
-import type { Sequelize, Transaction } from 'sequelize';
+import type { Transaction } from 'sequelize';
 import { LocString } from '@m-cafe-app/models';
 import { LocString as LocStringPG } from '@m-cafe-app/db';
 import { LocStringMapper } from '../infrastructure';
 import { DatabaseError } from '@m-cafe-app/utils';
 
 export class LocStringRepoSequelizePG implements ILocStringRepo {
-
-  private dbInstance: Sequelize;
-
-  constructor( readonly dbHandler: IDatabaseConnectionHandler ) {
-    if (!dbHandler.dbInstance) {
-      throw new DatabaseError('No database connection');
-    }
-    this.dbInstance = dbHandler.dbInstance;
-  }
 
   async getAll(): Promise<LocString[]> {
     const dbLocStrings = await LocStringPG.scope('all').findAll();
@@ -29,45 +19,30 @@ export class LocStringRepoSequelizePG implements ILocStringRepo {
     return LocStringMapper.dbToDomain(dbLocString);
   }
 
-  async create(locStringDTN: LocStringDTN, t?: Transaction): Promise<LocString> {
-    const createdLocString = t ?
-      await LocStringPG.create({
-        mainStr: locStringDTN.mainStr,
-        secStr: locStringDTN.secStr,
-        altStr: locStringDTN.altStr
-      }, {
-        transaction: t
-      }) :
-      await LocStringPG.create({
-        mainStr: locStringDTN.mainStr,
-        secStr: locStringDTN.secStr,
-        altStr: locStringDTN.altStr
-      });
+  async create(locStringDTN: LocStringDTN, transaction?: Transaction): Promise<LocString> {
+    const createdLocString = await LocStringPG.create({
+      mainStr: locStringDTN.mainStr,
+      secStr: locStringDTN.secStr,
+      altStr: locStringDTN.altStr
+    }, {
+      transaction
+    });
 
 
     return LocStringMapper.dbToDomain(createdLocString);
   }
 
-  async update(locString: LocString, t?: Transaction): Promise<LocString> {
-    const [ count, updated ] = t ?
-      await LocStringPG.update({
-        mainStr: locString.mainStr,
-        secStr: locString.secStr,
-        altStr: locString.altStr
-      },
-      {
-        where: { id: locString.id },
-        transaction: t,
-        returning: true
-      }) :
-      await LocStringPG.update({
-        mainStr: locString.mainStr,
-        secStr: locString.secStr,
-        altStr: locString.altStr
-      }, {
-        where: { id: locString.id },
-        returning: true
-      });
+  async update(locString: LocString, transaction?: Transaction): Promise<LocString> {
+    const [ count, updated ] = await LocStringPG.update({
+      mainStr: locString.mainStr,
+      secStr: locString.secStr,
+      altStr: locString.altStr
+    },
+    {
+      where: { id: locString.id },
+      transaction,
+      returning: true
+    });
 
     if (count === 0) {
       throw new DatabaseError(`No loc string entry with this id ${locString.id}`);
@@ -81,8 +56,8 @@ export class LocStringRepoSequelizePG implements ILocStringRepo {
     if (deletedCount === 0) throw new DatabaseError(`No loc string entry with this id ${id}`);
   }
 
-  async removeWithCount(id: number): Promise<number> {
-    return await LocStringPG.destroy({ where: { id } });
+  async removeWithCount(ids: number[]): Promise<number> {
+    return await LocStringPG.destroy({ where: { id: ids } });
   }
 
   async removeAll(): Promise<void> {
