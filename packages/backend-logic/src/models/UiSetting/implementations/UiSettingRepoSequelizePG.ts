8af33d0b34b475fa6b4a45581febf06cc291ae18
VasiliyTemniy <flash_er@mail.ru>
2023-11-16 18:09:05 +0300

    move transaction handling to service level

--- packages/backend-logic/src/models/UiSetting/implementations/UiSettingRepoSequelizePG.ts
+++ packages/backend-logic/src/models/UiSetting/implementations/UiSettingRepoSequelizePG.ts
@@ -1,23 +1,11 @@
-import type { UiSetting, UiSettingDTN } from '@m-cafe-app/models';
+import type { UiSetting, UiSettingDTN, UiSettingUniqiePropertiesGroup } from '@m-cafe-app/models';
 import type { IUiSettingRepo } from '../interfaces';
-import type { IDatabaseConnectionHandler } from '@m-cafe-app/db';
-import type { Sequelize } from 'sequelize';
+import type { Transaction } from 'sequelize';
 import { DatabaseError } from '@m-cafe-app/utils';
 import { UiSettingMapper } from '../infrastructure';
 import { UiSetting as UiSettingPG } from '@m-cafe-app/db';
 
 export class UiSettingRepoSequelizePG implements IUiSettingRepo {
-
-  private dbInstance: Sequelize;
-
-  constructor(
-    readonly dbHandler: IDatabaseConnectionHandler,
-  ) {
-    if (!dbHandler.dbInstance) {
-      throw new DatabaseError('No database connection');
-    }
-    this.dbInstance = dbHandler.dbInstance;
-  }
 
   async getAll(): Promise<UiSetting[]> {
     const dbUiSettings = await UiSettingPG.scope('all').findAll();
@@ -35,55 +23,45 @@ export class UiSettingRepoSequelizePG implements IUiSettingRepo {
     return UiSettingMapper.dbToDomain(dbUiSetting);
   }
 
-  async create(uiSettingDTN: UiSettingDTN): Promise<UiSetting> {
-    const createdUiSetting = await this.dbInstance.transaction(async (t) => {
-      try {
-        const dbUiSetting = await UiSettingPG.create({
-          name: uiSettingDTN.name,
-          value: uiSettingDTN.value,
-          theme: uiSettingDTN.theme,
-          group: uiSettingDTN.group
-        }, {
-          transaction: t
-        });
-
-        return UiSettingMapper.dbToDomain(dbUiSetting);
-      } catch (err) {
-        await t.rollback();
-        throw err;
-      }
-    });
-
-    return createdUiSetting;
+  async getByUniqueProperties(properties: UiSettingUniqiePropertiesGroup): Promise<UiSetting | undefined> {
+    const dbUiSetting = await UiSettingPG.scope('all').findOne({ where: properties });
+    if (!dbUiSetting) return undefined;
+    return UiSettingMapper.dbToDomain(dbUiSetting);
   }
 
-  async update(uiSetting: UiSetting): Promise<UiSetting> {
-    const updatedUiSetting = await this.dbInstance.transaction(async (t) => {
-      
-      const [ count, updated ] = await UiSettingPG.update({
-        value: uiSetting.value
-      }, {
-        where: {
-          id: uiSetting.id
-        },
-        transaction: t,
-        returning: true
-      });
-
-      if (count === 0) {
-        await t.rollback();
-        throw new DatabaseError(`No ui setting entry with this id ${uiSetting.id}`);
-      }
-
-      return UiSettingMapper.dbToDomain(updated[0]);
+  async create(uiSettingDTN: UiSettingDTN, transaction?: Transaction): Promise<UiSetting> {
+    const dbUiSetting = await UiSettingPG.create({
+      name: uiSettingDTN.name,
+      value: uiSettingDTN.value,
+      theme: uiSettingDTN.theme,
+      group: uiSettingDTN.group
+    }, {
+      transaction
     });
 
-    return updatedUiSetting;
+    return UiSettingMapper.dbToDomain(dbUiSetting);
   }
 
-  async updateMany(uiSettings: UiSetting[]): Promise<UiSetting[]> {
-    // Maybe add top-level transaction to prevent partial updating?
-    return await Promise.all(uiSettings.map(uiSetting => this.update(uiSetting)));
+  async update(uiSetting: UiSetting, transaction?: Transaction): Promise<UiSetting> {
+    const [ count, updated ] = await UiSettingPG.update({
+      value: uiSetting.value
+    }, {
+      where: {
+        id: uiSetting.id
+      },
+      transaction,
+      returning: true
+    });
+
+    if (count === 0) {
+      throw new DatabaseError(`No ui setting entry with this id ${uiSetting.id}`);
+    }
+
+    return UiSettingMapper.dbToDomain(updated[0]);
+  }
+
+  async updateMany(uiSettings: UiSetting[], transaction?: Transaction): Promise<UiSetting[]> {
+    return await Promise.all(uiSettings.map(uiSetting => this.update(uiSetting, transaction)));
   }
 
   async remove(id: number): Promise<void> {
