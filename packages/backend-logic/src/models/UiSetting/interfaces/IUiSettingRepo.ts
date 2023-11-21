53af48c461c9b837465b894fecb2903b672f9408
VasiliyTemniy <flash_er@mail.ru>
2023-11-21 19:18:51 +0300

    add 'getByUniqueProperties' to repo methods

--- packages/backend-logic/src/models/UiSetting/interfaces/IUiSettingRepo.ts
+++ packages/backend-logic/src/models/UiSetting/interfaces/IUiSettingRepo.ts
@@ -1,8 +1,9 @@
-import type { UiSetting, UiSettingDTN, UiSettingS } from '@m-cafe-app/models';
+import type { UiSetting, UiSettingDTN, UiSettingS, UiSettingUniqiePropertiesGroup } from '@m-cafe-app/models';
 import type { ICRUDRepo, IInmemRepo } from '../../../utils';
 
 export interface IUiSettingRepo extends ICRUDRepo<UiSetting, UiSettingDTN> {
   getByScope(scope: string): Promise<UiSetting[]>
+  getByUniqueProperties(properties: UiSettingUniqiePropertiesGroup): Promise<UiSetting | undefined>
 }
 
 export interface IUiSettingSRepo extends IInmemRepo<UiSetting, UiSettingS> {
