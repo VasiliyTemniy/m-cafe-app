e851bd39688971729803d87afc7e80865314e406
VasiliyTemniy <flash_er@mail.ru>
2023-11-21 19:16:00 +0300

    add getByIdWithComponents

--- packages/backend-logic/src/models/Food/interfaces/IFoodService.ts
+++ packages/backend-logic/src/models/Food/interfaces/IFoodService.ts
@@ -2,4 +2,5 @@ import type { FoodDT, FoodDTN } from '@m-cafe-app/models';
 import type { ICRUDService } from '../../../utils';
 
 export interface IFoodService extends ICRUDService<FoodDT, FoodDTN> {
+  getByIdWithComponents(id: number): Promise<FoodDT>;
 }
