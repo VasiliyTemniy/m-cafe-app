62702d9d732e60e38098dc9e97088cae5ca3c720
VasiliyTemniy <flash_er@mail.ru>
2023-11-21 19:28:48 +0300

    FixedLocDTN is used only for fixed locs init; must have no locString included

--- packages/models/src/dt/FixedLocDT.ts
+++ packages/models/src/dt/FixedLocDT.ts
@@ -1,8 +1,8 @@
 import type { MapToDT, MapToDTN, PropertyGroup } from '@m-cafe-app/utils'; 
 import type { FixedLoc, FixedLocS } from '../domain';
-import type { LocStringDTN, LocStringDT, LocStringDTS } from './LocStringDT';
-import { isEntity, isManyEntity, isString } from '@m-cafe-app/utils';
-import { idRequired, locStringProperty, locStringSimpleProperty, locStringNewProperty } from './validationHelpers.js';
+import type { LocStringDT, LocStringDTS } from './LocStringDT';
+import { isEntity, isString } from '@m-cafe-app/utils';
+import { idRequired, locStringProperty, locStringSimpleProperty } from './validationHelpers.js';
 
 
 const fixedLocPropertiesGroup: PropertyGroup = {
@@ -19,7 +19,7 @@ export const isFixedLocDT = (obj: unknown): obj is FixedLocDT =>
   isEntity(obj, [ idRequired, fixedLocPropertiesGroup, locStringProperty ]);
 
 export const isFixedLocDTMany = (obj: unknown): obj is FixedLocDT[] =>
-  isManyEntity(obj, isFixedLocDT);
+  Array.isArray(obj) && obj.every(isFixedLocDT);
   
 
 export type FixedLocDTS = Omit<MapToDT<FixedLocS>, 'locString'> & {
@@ -30,9 +30,11 @@ export const isFixedLocDTS = (obj: unknown): obj is FixedLocDTS =>
   isEntity(obj, [ fixedLocPropertiesGroup, locStringSimpleProperty ]);
 
 
-export type FixedLocDTN = Omit<MapToDTN<FixedLoc>, 'locString'> & {
-  locString: LocStringDTN;
-};
+// export type FixedLocDTN = Omit<MapToDTN<FixedLoc>, 'locString'> & {
+//   locString: LocStringDTN;
+// };
+
+export type FixedLocDTN = Omit<MapToDTN<FixedLoc>, 'locString'>;
 
 export const isFixedLocDTN = (obj: unknown): obj is FixedLocDTN =>
-  isEntity(obj, [ fixedLocPropertiesGroup, locStringNewProperty ]);
+  isEntity(obj, [ fixedLocPropertiesGroup ]);
