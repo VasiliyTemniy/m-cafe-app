ed898ebe38282e88a63668907b9c983708b53df4
VasiliyTemniy <flash_er@mail.ru>
2023-11-21 19:09:56 +0300

    rename create -> findOrCreate; add GenericTransaction as args to mutating queries

--- packages/backend-logic/src/models/Address/interfaces/IAddressRepo.ts
+++ packages/backend-logic/src/models/Address/interfaces/IAddressRepo.ts
@@ -1,20 +1,32 @@
 import type { Address, AddressDTN } from '@m-cafe-app/models';
-import type { ICRUDRepo } from '../../../utils';
-import type { Transaction } from 'sequelize';
+import type { GenericTransaction, ICRUDRepo } from '../../../utils';
 
 export interface IAddressRepo extends Omit<ICRUDRepo<Address, AddressDTN>, 'create' | 'update' | 'getAll' | 'remove'> {
   getById(id: number): Promise<Address>;
   getByUserId(userId: number): Promise<Address[]>;
+  findOrCreate(addressDTN: AddressDTN, transaction?: GenericTransaction): Promise<{ address: Address, created: boolean }>;
+  update(addressToUpdate: Address, transaction?: GenericTransaction): Promise<{ address: Address, updated: boolean }>;
+  removeIfUnused(addressId: number, transaction?: GenericTransaction): Promise<void>;
+  createUserAddress(
+    userId: number,
+    addressId: number,
+    transaction?: GenericTransaction
+  ): Promise<{ created: boolean }>;
+  updateUserAddress(
+    userId: number,
+    addressId: number,
+    oldAddressId: number,
+    transaction?: GenericTransaction
+  ): Promise<{ updated: boolean }>;
   /**
-   * Optionally accepts a transaction
+   * Remove user's address by addressId\
+   * Checks if address is still used by anything,\
+   * If not, deletes it and through-table record.\
+   * If yes, deletes only through-table record
    */
-  create(addressDTN: AddressDTN, t?: Transaction): Promise<{ address: Address, created: boolean }>;
-  /**
-   * Optionally accepts a transaction
-   */
-  update(addressToUpdate: Address, t?: Transaction): Promise<{ address: Address, updated: boolean }>;
-  removeIfUnused(addressId: number): Promise<void>;
-  createUserAddress(userId: number, addressId: number): Promise<{ created: boolean }>;
-  updateUserAddress(userId: number, addressId: number, oldAddressId: number): Promise<{ updated: boolean }>;
-  removeUserAddress(userId: number, addressId: number): Promise<void>;
+  removeUserAddress(
+    userId: number,
+    addressId: number,
+    transaction?: GenericTransaction
+  ): Promise<void>;
 }
