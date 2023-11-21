23c2f1550b6dcdb774bdce8d525823a81da0814c
VasiliyTemniy <flash_er@mail.ru>
2023-11-21 19:15:04 +0300

    change ICRUDRepo optional args to ...args: any

--- packages/backend-logic/src/utils/ICRUD.ts
+++ packages/backend-logic/src/utils/ICRUD.ts
@@ -1,17 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Request, Response } from 'express';
-import type { GenericTransaction } from './ITransactionHandler.js';
 
 export interface ICRUDRepo<T, DTN> {
   getAll(): Promise<T[]>;
   getById(id: number): Promise<T>;
-  create(dtn: DTN, transaction?: GenericTransaction, ...args: any): Promise<T>;
-  update(d: T, transaction?: GenericTransaction, ...args: any): Promise<T>;
-  remove(id: number, transaction?: GenericTransaction): Promise<void> | Promise<T>;
-  removeAll(transaction?: GenericTransaction): Promise<void>;
-  createMany?(dtns: DTN[], transaction?: GenericTransaction, ...args: any): Promise<T[]>;
-  updateMany?(ds: T[], transaction?: GenericTransaction, ...args: any): Promise<T[]>;
-  removeMany?(ids: number[], transaction?: GenericTransaction, ...args: any): Promise<void>;
+  create(dtn: DTN, ...args: any): Promise<T>;
+  update(d: T, ...args: any): Promise<T>;
+  remove(id: number, ...args: any): Promise<void> | Promise<T>;
+  removeAll(...args: any): Promise<void>;
+  createMany?(dtns: DTN[], ...args: any): Promise<T[]>;
+  updateMany?(ds: T[], ...args: any): Promise<T[]>;
+  removeMany?(ids: number[], ...args: any): Promise<void>;
 }
 
 export interface ICRUDService<DT, DTN> {
