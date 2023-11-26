f431b02229498fffbb79f789050f7b3a18ca1cbc
VasiliyTemniy <flash_er@mail.ru>
2023-11-26 21:56:16 +0300

    rename amount -> quantity; add archiveFoodId instead of food_id to primary key, because food could be deleted anytime; add some scope

--- packages/db/src/models/OrderFood.ts
+++ packages/db/src/models/OrderFood.ts
@@ -1,37 +1,33 @@
-import type { InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey, NonAttribute } from 'sequelize';
+import type { InferAttributes, InferCreationAttributes, ForeignKey, NonAttribute } from 'sequelize';
 import type { PropertiesCreationOptional } from '@m-cafe-app/shared-constants';
 import type { Sequelize } from 'sequelize';
 import { Model, DataTypes } from 'sequelize';
 import { Food } from './Food.js';
 import { Order } from './Order.js';
+import { includeFoodSimple } from './commonIncludes.js';
 
 export class OrderFood extends Model<InferAttributes<OrderFood>, InferCreationAttributes<OrderFood>> {
-  declare id: CreationOptional<number>;
   declare orderId: ForeignKey<Order['id']>;
   declare foodId: ForeignKey<Food['id']> | null;
-  declare amount: number;
+  declare archiveFoodId: number;
+  declare quantity: number;
   declare archivePrice: number;
   declare archiveFoodName: string;
   declare food?: NonAttribute<Food>;
 }
 
 
-export type OrderFoodData = Omit<InferAttributes<OrderFood>, PropertiesCreationOptional>
-  & { id: number; };
+export type OrderFoodData = Omit<InferAttributes<OrderFood>, PropertiesCreationOptional>;
 
 
 export const initOrderFoodModel = async (dbInstance: Sequelize) => {
   return new Promise<void>((resolve, reject) => {
     try {
       OrderFood.init({
-        id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
-        },
         orderId: {
           type: DataTypes.INTEGER,
           allowNull: false,
+          primaryKey: true,
           references: { model: 'orders', key: 'id' }
         },
         foodId: {
@@ -41,7 +37,12 @@ export const initOrderFoodModel = async (dbInstance: Sequelize) => {
           onUpdate: 'CASCADE',
           onDelete: 'SET NULL'
         },
-        amount: {
+        archiveFoodId: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          allowNull: false
+        },
+        quantity: {
           type: DataTypes.INTEGER,
           allowNull: false
         },
@@ -57,7 +58,12 @@ export const initOrderFoodModel = async (dbInstance: Sequelize) => {
         sequelize: dbInstance,
         underscored: true,
         timestamps: false,
-        modelName: 'order_food'
+        modelName: 'order_food',
+        scopes: {
+          withFoodSimple: {
+            include: includeFoodSimple
+          }
+        }
       });
 
       resolve();
