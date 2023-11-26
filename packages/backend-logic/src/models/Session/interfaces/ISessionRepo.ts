48930a1142558b6a71a938a2574060c3caed1feb
VasiliyTemniy <flash_er@mail.ru>
2023-11-26 21:53:56 +0300

    upd because InmemRepoBase dictates removeAll without ...args: any. Maybe will be changed

--- packages/backend-logic/src/models/Session/interfaces/ISessionRepo.ts
+++ packages/backend-logic/src/models/Session/interfaces/ISessionRepo.ts
@@ -2,7 +2,7 @@ import type { AuthResponse, Session } from '@m-cafe-app/models';
 import type { ICRUDRepo, IInmemRepoBase } from '../../../utils';
 
 // SessionDT, SessionDTN does not exist so the arguments are Session
-export interface ISessionRepo extends Omit<ICRUDRepo<Session, Session>, 'remove' | 'getAll' | 'getById'>, IInmemRepoBase {
+export interface ISessionRepo extends Omit<ICRUDRepo<Session, Session>, 'remove' | 'getAll' | 'getById' | 'removeAll'>, IInmemRepoBase {
   getAllByUserId(userId: number): Promise<Session[]>;
   getOne(userId: number, userAgentHash: string): Promise<Session | undefined>;
   remove(userId: number, userAgentHash?: string): Promise<void>;
