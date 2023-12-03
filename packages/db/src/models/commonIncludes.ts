0a5aff81947537a9f08213f042847dc20f27c2bb
VasiliyTemniy <flash_er@mail.ru>
2023-12-03 12:32:40 +0300

    add includes for FoodPicture, FoodComponent

--- packages/db/src/models/commonIncludes.ts
+++ packages/db/src/models/commonIncludes.ts
@@ -2,6 +2,9 @@ import { Address } from './Address.js';
 import { LocString } from './LocString.js';
 import { Food } from './Food.js';
 import { Ingredient } from './Ingredient.js';
+import { FoodComponent } from './FoodComponent.js';
+import { FoodPicture } from './FoodPicture.js';
+import { Picture } from './Picture.js';
 
 export const includeAddress = {
   model: Address,
@@ -51,11 +54,19 @@ export const includeStockMeasureLoc = {
   }
 };
 
+export const includeAltTextLoc = {
+  model: LocString,
+  as: 'altTextLoc',
+  attributes: {
+    exclude: ['createdAt', 'updatedAt']
+  }
+};
+
 
 export const includeFoodComponentData = [
   {
     model: Food,
-    as: 'food',
+    as: 'food', // as: 'component' ??? No, it is mapped to 'component' in afterFind hook
     attributes: {
       exclude: ['nameLocId', 'descriptionLocId', 'foodTypeId', 'price', 'createdAt', 'updatedAt']
     },
@@ -78,7 +89,7 @@ export const includeFoodComponentData = [
   },
   {
     model: Ingredient,
-    as: 'ingredient',
+    as: 'ingredient', // as: 'component' ??? No, it is mapped to 'component' in afterFind hook
     attributes: {
       exclude: ['nameLocId', 'stockMeasureLocId', 'createdAt', 'updatedAt']
     },
@@ -100,6 +111,56 @@ export const includeFoodComponentData = [
     ]
   }
 ];
+
+
+export const includeFoodComponents = {
+  model: FoodComponent,
+  as: 'foodComponents',
+  include: includeFoodComponentData,
+  attributes: {
+    exclude: ['createdAt', 'updatedAt']
+  }
+};
+
+
+export const includeFoodMainPicture = {
+  model: FoodPicture,
+  as: 'mainPicture',
+  where: {
+    orderNumber: 0
+  },
+  include: [
+    {
+      model: Picture,
+      as: 'picture',
+      attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      },
+      include: [
+        includeAltTextLoc
+      ]
+    }
+  ]
+};
+
+
+export const includeFoodGallery = {
+  model: FoodPicture,
+  as: 'gallery',
+  include: [
+    {
+      model: Picture,
+      as: 'picture',
+      attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      },
+      include: [
+        includeAltTextLoc
+      ]
+    }
+  ]
+};
+
 
 export const includeFoodSimple = {
   model: Food,
