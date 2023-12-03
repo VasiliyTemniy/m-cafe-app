62a554e65a7e352c9cb3c622d8d83e619da019e9
VasiliyTemniy <flash_er@mail.ru>
2023-12-03 12:34:15 +0300

    reexport commonIncludes

--- packages/db/src/models/index.ts
+++ packages/db/src/models/index.ts
@@ -22,3 +22,4 @@ export * from './initFacilityScopes.js';
 export * from './initUserScopes.js';
 export * from './initFoodScopes.js';
 export * from './initOrderScopes.js';
+export * from './commonIncludes.js';
