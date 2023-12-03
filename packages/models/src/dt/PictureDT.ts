44a89894e70458f9e17c3c8209146526f35f797f
VasiliyTemniy <flash_er@mail.ru>
2023-12-03 12:35:58 +0300

    separate new picture for food and new picture for dynamic module

--- packages/models/src/dt/PictureDT.ts
+++ packages/models/src/dt/PictureDT.ts
@@ -2,7 +2,7 @@ import type { MapToDT, PropertyGroup } from '@m-cafe-app/utils';
 import type { Picture } from '../domain';
 import type { LocStringDT } from './LocStringDT';
 import { isLocStringDT } from './LocStringDT.js';
-import { isString, isEntity, checkProperties } from '@m-cafe-app/utils';
+import { isString, isEntity, checkProperties, isUnknownObject } from '@m-cafe-app/utils';
 import { idRequired } from './validationHelpers';
 
 const picturePropertiesGroups: PropertyGroup[] = [{
@@ -21,32 +21,52 @@ export const isPictureDT = (obj: unknown): obj is PictureDT =>
   isEntity(obj, [ idRequired, ...picturePropertiesGroups ]);
 
 
-/**
- * This data transit new is not connected with PictureDT because picture file is
- * sent via multipart/form-data, so all body fields are pure text fields
- */
-export type PictureDTN = {
-  type: 'foodPicture' | 'modulePicture',
-  orderNumber?: string,
+export type PictureForFoodDTN = {
   altTextMainStr: string,
   altTextSecStr?: string,
   altTextAltStr?: string,
-  subjectId: string
+  foodId: string,
+  orderNumber: string
 };
 
-export const isPictureDTN = (obj: unknown): obj is PictureDTN => {
+export const isPictureForFoodDTN = (obj: unknown): obj is PictureForFoodDTN => {
+
+  if (!isUnknownObject(obj)) return false;
 
   if (!checkProperties({ obj, properties: [
-    'altTextMainStr', 'subjectId', 'type'
+    'altTextMainStr', 'foodId', 'orderNumber'
   ], required: true, validator: isString })) return false;
+
+  if (isNaN(Number(obj.orderNumber)) || isNaN(Number(obj.foodId))) return false;
 
   if (!checkProperties({ obj, properties: [
     'altTextSecStr', 'altTextAltStr'
   ], required: false, validator: isString })) return false;
 
+  return true;
+};
+
+
+export type PictureForDynamicModuleDTN = {
+  altTextMainStr: string,
+  altTextSecStr?: string,
+  altTextAltStr?: string,
+  dynamicModuleId: string
+};
+
+export const isPictureForDynamicModuleDTN = (obj: unknown): obj is PictureForDynamicModuleDTN => {
+
+  if (!isUnknownObject(obj)) return false;
+
   if (!checkProperties({ obj, properties: [
-    'orderNumber'
-  ], required: false, validator: (value) => !isNaN(Number(value)) })) return false;
+    'altTextMainStr', 'dynamicModuleId'
+  ], required: true, validator: isString })) return false;
+
+  if (isNaN(Number(obj.orderNumber)) || isNaN(Number(obj.foodId))) return false;
+
+  if (!checkProperties({ obj, properties: [
+    'altTextSecStr', 'altTextAltStr'
+  ], required: false, validator: isString })) return false;
 
   return true;
 };
