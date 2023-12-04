c0e7222f600ee83ef85ccd1bd2fa31f890df952a
VasiliyTemniy <flash_er@mail.ru>
2023-12-04 19:35:45 +0300

    upd config - leave only server-related vars

--- services/backend/src/utils/config.ts
+++ services/backend/src/utils/config.ts
@@ -1,8 +1,4 @@
-import type { RedisClientOptions, RedisFunctions, RedisModules, RedisScripts } from '@redis/client';
-import type { CookieOptions } from 'express';
-import type { Secret } from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
-import { readFileSync } from 'fs';
 
 export const isDockerized = (process.env.DOCKERIZED_DEV === 'true' || process.env.DOCKERIZED === 'true');
 
@@ -11,55 +7,6 @@ dotenv.config({
 });
 
 
-const PORT = process.env.BACKEND_PORT as string;
+export const PORT = process.env.BACKEND_PORT as string;
 
-const SECRET = process.env.SECRET as Secret;
-
-const TOKEN_TTL = process.env.TOKEN_TTL as string;
-
-const ALLOWED_ORIGIN = (process.env.ALLOWED_ORIGIN as string).split(',');
-
-const SUPERADMIN_PHONENUMBER = process.env.SUPERADMIN_PHONENUMBER as string;
-
-
-const redisUseTLS = process.env.REDIS_TLS as string === 'true';
-
-const redisConfig: RedisClientOptions<RedisModules, RedisFunctions, RedisScripts> = {
-
-  username: process.env.REDIS_USERNAME as string,
-  password: process.env.REDIS_PASSWORD as string,
-  socket: {
-    host: process.env.REDIS_HOST as string,
-    port: Number(process.env.REDIS_PORT as string),
-    tls: redisUseTLS,
-    key: redisUseTLS ? readFileSync('./redis_user_private.key') : undefined,
-    cert: redisUseTLS ? readFileSync('./redis_user.crt') : undefined,
-    ca: redisUseTLS ? [readFileSync('./redis_ca.pem')] : undefined
-  },
-
-};
-
-
-const cookieExpiracyMS =
-  TOKEN_TTL.endsWith('s') ? Number(TOKEN_TTL.slice(0, TOKEN_TTL.length - 1)) * 1000 :
-  TOKEN_TTL.endsWith('m') ? Number(TOKEN_TTL.slice(0, TOKEN_TTL.length - 1)) * 60 * 1000 :
-  TOKEN_TTL.endsWith('h') ? Number(TOKEN_TTL.slice(0, TOKEN_TTL.length - 1)) * 60 * 60 * 1000 :
-  TOKEN_TTL.endsWith('d') ? Number(TOKEN_TTL.slice(0, TOKEN_TTL.length - 1)) * 24 * 60 * 60 * 1000
-  : Number(TOKEN_TTL);
-
-const sessionCookieOptions: CookieOptions = {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production'
-};
-
-
-export default {
-  PORT,
-  SECRET,
-  TOKEN_TTL,
-  ALLOWED_ORIGIN,
-  SUPERADMIN_PHONENUMBER,
-  redisConfig,
-  sessionCookieOptions,
-  cookieExpiracyMS
-};
+export const ALLOWED_ORIGIN = (process.env.ALLOWED_ORIGIN as string).split(',');
