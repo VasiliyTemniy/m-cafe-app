b98c3b04d31339ae4d067c32fde3413535f07757
VasiliyTemniy <flash_er@mail.ru>
2023-12-09 18:38:35 +0300

    improve uiSettings redis db data handling

--- packages/backend-logic/src/models/UiSetting/implementations/UiSettingRepoRedis.ts
+++ packages/backend-logic/src/models/UiSetting/implementations/UiSettingRepoRedis.ts
@@ -1,24 +1,40 @@
 import type { UiSetting, UiSettingS } from '@m-cafe-app/models';
 import type { IUiSettingSRepo } from '../interfaces';
 import { RedisRepoBase } from '../../../utils';
+import { allowedThemesReadonly } from '@m-cafe-app/shared-constants';
 
 export class UiSettingRepoRedis extends RedisRepoBase implements IUiSettingSRepo {
 
   async getMany(theme?: string): Promise<UiSettingS[]> {
+    if (theme) return this.getByTheme(theme);
+
+    let uiSettingsInmem: UiSettingS[] = [];
+
+    for (const theme of allowedThemesReadonly) {
+      const themedUiSettings = await this.getByTheme(theme);
+      uiSettingsInmem = uiSettingsInmem.concat(themedUiSettings);
+    }
+
+    return uiSettingsInmem;
+  }
+
+  private async getByTheme(theme: string): Promise<UiSettingS[]> {
     const uiSettingsInmem: UiSettingS[] = [];
 
-    for await (const key of this.redisClient.scanIterator()) {
-      const response = await this.redisClient.hGetAll(key);
+    const keys = await this.redisClient.keys(`${theme}:*`);
 
-      if (theme && response.theme !== theme)
-        continue;
-
-      uiSettingsInmem.push({
-        name: response.name,
-        value: response.value,
-        group: response.group,
-        theme: response.theme
-      });
+    for (const key of keys) {
+      const result = await this.redisClient.hGetAll(key);
+      const group = key.split(':')[1];
+  
+      for (const resultKey in result) {
+        uiSettingsInmem.push({
+          name: resultKey,
+          value: result[resultKey],
+          group,
+          theme
+        });
+      }
     }
 
     return uiSettingsInmem;
@@ -28,7 +44,7 @@ export class UiSettingRepoRedis extends RedisRepoBase implements IUiSettingSRepo
     const multi = this.redisClient.multi();
 
     for (const uiSetting of uiSettings) {
-      multi.hSet(`${String(uiSetting.id)}`, { name: uiSetting.name, value: uiSetting.value, group: uiSetting.group, theme: uiSetting.theme });
+      multi.hSet(`${uiSetting.theme}:${uiSetting.group}`, uiSetting.name, uiSetting.value);
     }
 
     await multi.exec();
