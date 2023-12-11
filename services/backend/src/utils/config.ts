38831af0c4b0ef6d8b530f90c9f2a48918ceb93c
VasiliyTemniy <flash_er@mail.ru>
2023-12-11 16:54:26 +0300

    add fixed_locs_path to env and config of a backend service

--- services/backend/src/utils/config.ts
+++ services/backend/src/utils/config.ts
@@ -10,3 +10,11 @@ dotenv.config({
 export const PORT = process.env.BACKEND_PORT as string;
 
 export const ALLOWED_ORIGIN = (process.env.ALLOWED_ORIGIN as string).split(',');
+
+
+export const FIXED_LOCS_PATH = process.env.FIXED_LOCS_PATH || 'locales';
+
+export const FIXED_LOCS_EXT =
+  process.env.FIXED_LOCS_EXT === 'json' ? 'json' :
+  process.env.FIXED_LOCS_EXT === 'jsonc' ? 'jsonc' :
+  'jsonc';
