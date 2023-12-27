91ed640da2c88218147472b17d27f05bc3aa768d
VasiliyTemniy <flash_er@mail.ru>
2023-12-27 14:31:22 +0300

    add Currency model with conversions submodel

---
+++ packages/db/src/models/Currency.ts
@@ -0,0 +1,84 @@
+import type {
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  NonAttribute,
+  Sequelize
+} from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
+import { LocParentType, LocType } from '@m-cafe-app/shared-constants';
+import { Loc } from './Loc.js';
+
+
+export class Currency extends Model<InferAttributes<Currency>, InferCreationAttributes<Currency>> {
+  declare id: CreationOptional<number>;
+  declare code: string;
+  declare decimalMultiplier: number;
+  declare nameLocs?: NonAttribute<Loc>[];
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+
+export const initCurrencyModel = async (dbInstance: Sequelize) => {
+  return new Promise<void>((resolve, reject) => {
+    try {
+      Currency.init({
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        code: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        decimalMultiplier: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false
+        }
+        // actual locs are referenced from locs table
+      }, {
+        sequelize: dbInstance,
+        underscored: true,
+        timestamps: true,
+        modelName: 'currency',
+      });
+      
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+
+export const initCurrencyAssociations = async () => {
+  return new Promise<void>((resolve, reject) => {
+    try {
+
+      Currency.hasMany(Loc, {
+        foreignKey: 'parentId',
+        as: 'nameLocs',
+        scope: {
+          parentType: LocParentType.Currency,
+          locType: LocType.Name
+        },
+        constraints: false,
+        foreignKeyConstraint: false
+      });
+
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
---
+++ packages/db/src/models/CurrencyConversion.ts
@@ -0,0 +1,92 @@
+import type {
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  NonAttribute,
+  ForeignKey,
+  Sequelize
+} from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
+import { Currency } from './Currency.js';
+
+
+export class CurrencyConversion extends Model<InferAttributes<CurrencyConversion>, InferCreationAttributes<CurrencyConversion>> {
+  declare sourceId: ForeignKey<Currency['id']>;
+  declare targetId: ForeignKey<Currency['id']>;
+  declare rate: number;
+  declare sourceCurrency?: NonAttribute<Currency>;
+  declare targetCurrency?: NonAttribute<Currency>;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+
+export const initCurrencyConversionModel = async (dbInstance: Sequelize) => {
+  return new Promise<void>((resolve, reject) => {
+    try {
+      CurrencyConversion.init({
+        sourceId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          references: { model: 'currencies', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        },
+        targetId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          references: { model: 'currencies', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        },
+        rate: {
+          type: DataTypes.DOUBLE,
+          allowNull: false
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false
+        }
+      }, {
+        sequelize: dbInstance,
+        underscored: true,
+        timestamps: true,
+        modelName: 'currency_conversion',
+      });
+      
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+
+export const initCurrencyConversionAssociations = async () => {
+  return new Promise<void>((resolve, reject) => {
+    try {
+
+      CurrencyConversion.belongsTo(Currency, {
+        foreignKey: 'sourceId',
+        targetKey: 'id',
+        as: 'sourceCurrency'
+      });
+
+      CurrencyConversion.belongsTo(Currency, {
+        foreignKey: 'targetId',
+        targetKey: 'id',
+        as: 'targetCurrency'
+      });
+
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
