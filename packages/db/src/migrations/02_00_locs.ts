1e60b7c1525557915b752c4d5364f8417205eb49
VasiliyTemniy <flash_er@mail.ru>
2023-12-28 16:56:54 +0300

    split locs table to locs and loc_references table to exclude duplicates

--- packages/db/src/migrations/02_00_locs.ts
+++ packages/db/src/migrations/02_00_locs.ts
@@ -1,62 +1,34 @@
 import type { MigrationContext } from '../types/Migrations.js';
-import { DataTypes, QueryTypes } from 'sequelize';
-import { LocParentType, LocType } from '@m-cafe-app/shared-constants';
+import { DataTypes } from 'sequelize';
 
 export const up = async ({ context: queryInterface }: MigrationContext) => {
   await queryInterface.createTable('locs', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     language_id: {
       type: DataTypes.INTEGER,
       references: { model: 'languages', key: 'id' },
       allowNull: false,
       onUpdate: 'CASCADE',
-      onDelete: 'CASCADE'
-    },
-    loc_type: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isIn: [Object.values(LocType)]
-      }
-    },
-    parent_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    parent_type: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isIn: [Object.values(LocParentType)]
-      }
+      onDelete: 'CASCADE',
+      unique: 'unique_loc'
     },
     text: {
       type: DataTypes.STRING(5000),
-      allowNull: false
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: false
+      allowNull: false,
+      unique: 'unique_loc'
+    }
+  }, {
+    uniqueKeys: {
+      unique_loc: {
+        customIndex: true,
+        fields: ['language_id', 'text']
+      }
     }
   });
-
-  const constraintCheck = await queryInterface.sequelize.query(
-    "SELECT * FROM pg_constraint WHERE conname = 'locs_pkey'",
-    { type: QueryTypes.SELECT }
-  );
-
-  if (!constraintCheck[0]) {
-    await queryInterface.addConstraint('locs', {
-      fields: [
-        'language_id', 'loc_type', 'parent_id', 'parent_type'
-      ],
-      type: 'primary key',
-      name: 'locs_pkey'
-    });
-  }
 };
 
 export const down = async ({ context: queryInterface }: MigrationContext) => {
---
+++ packages/db/src/migrations/02_01_loc_references.ts
@@ -0,0 +1,52 @@
+import type { MigrationContext } from '../types/Migrations.js';
+import { DataTypes, QueryTypes } from 'sequelize';
+import { LocParentType, LocType } from '@m-cafe-app/shared-constants';
+
+export const up = async ({ context: queryInterface }: MigrationContext) => {
+  await queryInterface.createTable('loc_references', {
+    loc_id: {
+      type: DataTypes.INTEGER,
+      references: { model: 'locs', key: 'id' },
+      allowNull: false,
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    loc_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [Object.values(LocType)]
+      }
+    },
+    parent_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    parent_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [Object.values(LocParentType)]
+      }
+    }
+  });
+
+  const constraintCheck = await queryInterface.sequelize.query(
+    "SELECT * FROM pg_constraint WHERE conname = 'loc_references_pkey'",
+    { type: QueryTypes.SELECT }
+  );
+
+  if (!constraintCheck[0]) {
+    await queryInterface.addConstraint('loc_references', {
+      fields: [
+        'loc_id', 'loc_type', 'parent_id', 'parent_type'
+      ],
+      type: 'primary key',
+      name: 'loc_references_pkey'
+    });
+  }
+};
+
+export const down = async ({ context: queryInterface }: MigrationContext) => {
+  await queryInterface.dropTable('loc_references');
+};
