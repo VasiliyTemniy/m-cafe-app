d23e7da682065ffc5ebe3149ee5da1fac9f7f074
VasiliyTemniy <flash_er@mail.ru>
2024-01-15 22:20:32 +0300

    validators slight refactor; add checks and early returns for optionals; add obj property counters

--- packages/utils/src/types/typeValidators.ts
+++ packages/utils/src/types/typeValidators.ts
@@ -36,20 +36,26 @@ export const parseJSON = (strJson: unknown): JSON => {
   return strJson;
 };
 
-export const isBoolean = (bool: unknown): bool is boolean => {
+export const isBoolean = (bool: unknown, optional: boolean = false): bool is boolean => {
+  if (optional) if (bool === null || bool === undefined) return true;
   return typeof bool === 'boolean' || bool instanceof Boolean;
 };
 
-export const isString = (text: unknown): text is string => {
+export const isString = (text: unknown, optional: boolean = false): text is string => {
+  if (optional) if (text === null || text === undefined) return true;
   return typeof text === 'string' || text instanceof String;
 };
 
-export const isNumber = (num: unknown): num is number => {
+export const isNumber = (num: unknown, optional: boolean = false): num is number => {
+  if (optional) if (num === null || num === undefined) return true;
   return typeof num === 'number' || num instanceof Number;
 };
 
-export const isDate = (date: unknown): boolean => {
-  return Boolean(Date.parse(date as string));
+export const isDate = (date: unknown, optional: boolean = false): boolean => {
+  if (optional) if (date === null || date === undefined) return true;
+  if (date instanceof Date) return true;
+  if (!isString(date)) return false;
+  return Boolean(Date.parse(date));
 };
 
 export const isJSON = (json: unknown): json is JSON => {
@@ -79,22 +85,23 @@ export const isUnknownArray = (arr: unknown): arr is UnknownArray => {
 };
 
 interface CheckPropertiesArgs {
-  obj: unknown;
+  obj: UnknownObject;
   properties: string[];
   required: boolean;
-  validator: (value: unknown) => boolean;
+  validator: (value: unknown, optional?: boolean) => boolean;
   isArray?: boolean;
 }
 
 /**
- * Checks if an object has the specified properties and validates their values.
+ * Checks if an object has the specified properties and validates their values.\
+ * Also counts the number of checked properties.
  *
- * @param {Object} obj - The object to be checked.
+ * @param {UnknownObject} obj - The object to be checked.
  * @param {string[]} properties - The properties to check for.
  * @param {boolean} required - Indicates if the properties are required.
  * @param {Function} validator - The function used to validate the property values.
  * @param {boolean} isArray - Indicates if the property values should be arrays.
- * @return {boolean} Returns true if all properties are present and their values are valid, otherwise returns false.
+ * @return returns { result: boolean, checkedCount: number }
  */
 export const checkProperties = ({
   obj,
@@ -102,36 +109,44 @@ export const checkProperties = ({
   required,
   validator,
   isArray = false
-}: CheckPropertiesArgs): boolean => {
-  if (!isUnknownObject(obj)) return false;
+}: CheckPropertiesArgs): { result: boolean, checkedCount: number } => {
+
+  let checkedCount = 0;
+
   if (required && !isArray) {
-    if (!hasOwnProperties(obj, properties)) return false;
+    if (!hasOwnProperties(obj, properties)) return { result: false, checkedCount };
     for (const property of properties) {
-      if (!validator(obj[property])) return false;
+      if (!validator(obj[property])) return { result: false, checkedCount };
+      checkedCount++;
     }
   } else if (required && isArray) {
-    if (!hasOwnProperties(obj, properties)) return false;
+    if (!hasOwnProperties(obj, properties)) return { result: false, checkedCount };
     for (const property of properties) {
-      if (!Array.isArray(obj[property])) return false;
+      if (!Array.isArray(obj[property])) return { result: false, checkedCount };
       const arr = obj[property] as Array<unknown>;
-      for (const item of arr)
-        if (!validator(item)) return false;
+      for (const item of arr) {
+        if (!validator(item)) return { result: false, checkedCount };
+      }
+      checkedCount++;
     }
   } else if (!required && !isArray) {
     for (const property of properties) {
       if (!hasOwnProperty(obj, property)) continue;
-      if (!validator(obj[property])) return false;
+      if (!validator(obj[property], true)) return { result: false, checkedCount };
+      checkedCount++;
     }
   } else if (!required && isArray) {
     for (const property of properties) {
       if (!hasOwnProperty(obj, property)) continue;
-      if (!Array.isArray(obj[property])) return false;
+      if (!Array.isArray(obj[property])) return { result: false, checkedCount };
       const arr = obj[property] as Array<unknown>;
-      for (const item of arr)
-        if (!validator(item)) return false;
+      for (const item of arr) {
+        if (!validator(item, true)) return { result: false, checkedCount };
+      }
+      checkedCount++;
     }
   }
-  return true;
+  return { result: true, checkedCount };
 };
 
 
@@ -143,13 +158,15 @@ export const checkProperties = ({
 export interface PropertyGroup {
   properties: string[];
   required?: boolean;
-  validator: (value: unknown) => boolean;
+  validator: (value: unknown, optional?: boolean) => boolean;
   isArray?: boolean;
 }
 
 /**
  * Checks if the given object is an entity by validating its properties against the provided PropertyGroups.
- * By default, properties are required and are not arrays.
+ * By default, properties are required and are not arrays.\
+ * If the object has more properties than those checked in validation process, then this object is suspicious.
+ * The function does not throw an Error in this case, though.
  *
  * @param {unknown} obj - The object to be checked.
  * @param {PropertyGroup[]} propertiesGroups - An array of PropertyGroups containing the properties, validators, and other configurations.
@@ -157,22 +174,28 @@ export interface PropertyGroup {
  */
 export const isEntity = <T>(obj: unknown, propertiesGroups: PropertyGroup[]): obj is T => {
   if (!isUnknownObject(obj)) return false;
+  let totalCheckedCount = 0;
   for (const propertyGroup of propertiesGroups) {
     const { properties, required = true, validator, isArray = false } = propertyGroup;
-    if (!checkProperties({
+    const { result, checkedCount } = checkProperties({
       obj,
       properties,
       required,
       validator,
       isArray
-    })) return false;
+    });
+    if (!result) return false;
+    totalCheckedCount += checkedCount;
   }
+  // obj cannot have less properties than those already checked;
+  // If totalCheckedCount is more than quantity of object keys, than this object is suspicious
+  // Suggestion: throw an error here?
+  if (totalCheckedCount !== Object.keys(obj).length) return false;
   return true;
 };
 
 export const isManyEntity = <T>(obj: unknown, validator: (value: unknown) => boolean): obj is T[] => {
   if (!isUnknownArray(obj)) return false;
-  if (!Array.isArray(obj)) return false;
   for (const item of obj) {
     if (!validator(item)) return false;
   }
